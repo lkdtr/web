ed00c5e05ff3c7283f278cc2f9b24caedf97239b
doruk.fisek@linux.org.tr <doruk.fisek@linux.org.tr@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2009-02-12 07:17:41 +0000

    Flash menu calissin

---
+++ senlik.linux.org.tr/2008/index.php
@@ -0,0 +1,40 @@
+<?php
+ switch ($_GET['link'])
+  {
+   case 'haberler.php':
+    header('Location: index1489.html');
+    exit;
+   case 'onkayit.php':
+    header('Location: index176a.html');
+    exit;
+   case 'hakkinda.php':
+    header('Location: index35c8.html');
+    exit;
+   case 'program.php':
+    header('Location: index4636.html');
+    exit;
+   case 'sponsorluk.php':
+    header('Location: indexbbec.html');
+    exit;
+   case 'cagri.php':
+    header('Location: indexfe20.html');
+    exit;
+   case 'konaklama.php':
+    header('Location: indexe541.html');
+    exit;
+   case 'gonullu.php':
+    header('Location: index88bf.html');
+    exit;
+   case 'afis.php':
+    header('Location: index5a28.html');
+    exit;
+   case 'eski.php':
+    header('Location: indexeb03.html');
+    exit;
+   case 'iletisim.php':
+    header('Location: index643.html');
+    exit;
+   default :
+    header('Location: index.html');    
+  }
+?>

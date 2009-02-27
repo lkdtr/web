57e70bbaa658c83bcec220ea805e0795b2ea0f1d
bahri.meric.canli <bahri.meric.canli@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2009-02-27 11:37:23 +0000

    olmadi bidaha

--- www.linux.org.tr/includes.php
+++ www.linux.org.tr/includes.php
@@ -5,4 +5,9 @@ $dagitim="extras/";
 $jscript="jscript/";
 $stil="stil/";
 $linuxfirma="/linuxfirmalar_files/";
+
+if($_GET['goster']=='google13ba1e19c2099d12') { // google dogrulama
+	exit;
+}
+
 ?>

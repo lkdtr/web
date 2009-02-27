04cb82bc0d37fb0631eb0f4b91aca964263be91e
Bahri Meriç CANLI <bahri@bahri.info>
2009-02-27 03:48:21 +0000

    welcome to 2009

---
+++ odul.linux.org.tr/2009/tpl/goster.kategori.tpl.php
@@ -0,0 +1,20 @@
+<!-- start content -->
+<center>
+<table cellpadding="0" cellspacing="0" border="0" width="691" height="700" colspan="2">
+<tr><td valign="top" witdh="691" height="146" colspan="2" background="images/partion1.jpg"></td></tr>
+<tr><td valign="top" witdh="691" height="180" colspan="2" background="images/partion2.jpg"></td></tr>
+<tr><td colspan="1" valign="top">
+<table cellpadding="0" cellspacing="0" width="320" valign="top" height="100%">
+<tr>
+<td width="11">&nbsp;</td>
+<td valign="top" bgcolor="#f7f7f7">
+<table cellpadding="0" cellspacing="6" width="100%" valign="top"><tr><td>
+<font>
+<?php goster_kategori($_GET['id']); ?>
+</font></td></table>
+</td></table>
+</td>
+<td colspan="1" cellpadding="0" cellspacing="0" bgcolor="#f7f7f7" valign="top"><img src="images/partion3.jpg" usemap="#menu" border="0"></td>
+</table>
+</center>
+<!-- end content -->

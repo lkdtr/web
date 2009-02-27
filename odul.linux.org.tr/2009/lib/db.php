17603bf8528a5dc49bf4e95e97e9970675a1beba
bahri.meric.canli <bahri.meric.canli@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2009-02-27 03:48:21 +0000

    welcome to 2009

---
+++ odul.linux.org.tr/2009/lib/db.php
@@ -0,0 +1,25 @@
+<?php
+if(DBCTYPE == "p"){$db_connection = @mysql_pconnect(DBHOST,DBUSER,DBPASS);}
+else{$db_connection = @mysql_connect(DBHOST,DBUSER,DBPASS);}
+
+if(!$db_connection){
+echo "Veritabanı sunucusuna bağlanamadm. Bir hata oluştu.<br>";
+echo "Hata mesaj: <b>".mysql_error()."</b><br>";
+echo "Hata numaras: <b>".mysql_errno()."</b><br>";
+exit();
+}
+
+$db_select = @mysql_select_db(DBNAME);
+
+if(!$db_select){
+echo "Veritabanına bağlantı kuramadım. Bir hata oluştu.<br>";
+echo "Hata mesaj: <b>".mysql_error()."</b><br>";
+echo "Hata numaras: <b>".mysql_errno()."</b><br>";
+exit();
+}
+else {
+
+mysql_query("SET CHARACTER SET LATIN5", $db_connection);
+
+}
+?>
---
+++ odul.linux.org.tr/2009/yonetim/tpl/onayla.aday.tpl.php
@@ -0,0 +1,9 @@
+<!-- start content -->
+  <tr bordercolor="#cccccc">
+    <td bgcolor="#d6dde7">
+    <p></p>
+    <?php onayla_aday_liste(); ?>
+    <p></p>
+    </td>
+  </tr>
+<!-- end content -->

c3341a265ab373120e766501ed34c25091aedc37
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2009-03-02 11:59:37 +0000

    2005 gelir hos gelir ley ley...

---
+++ senlik.linux.org.tr/2005/templates/lkd_senlik2005/rss.php
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title>Powered by LKD Etkinlik-cg</title>
+	<link>http://senlik.linux.org.tr/2005b</link>
+	<description>IV. Linux and Open Source Festival Site Syndication</description>
+	<managingEditor>erdinc@erdinc.info (Ali Erdinç Köroğlu)</managingEditor>
+	<language>tr-TR</language>
+	<generator>AEK RSS Generator 1.0.2</generator>
+	<item>
+		<title>Ve Şenlik bitti..</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=78ampItemid=2</link>
+		<description>Şenlik, dördüncü ve son günü 17:00'de başlayan kapanış töreni ve kokteyli
+ile son buldu. Dernek başkanı Mustafa Akgül ve etkinlik çalışma grubu adına
+Ali Erdinç Köroğlu'nun yaptığı kapanış konuşmalarının ardından şenlik
+sponsorlarına plaketleri verildi. &lt;br /&gt;&lt;br /&gt;Şenlikte çalışan görevli
+penguenler teker teker sahneye çağrılarak teşekkür belgeleri iletilirken, yıl
+boyunca Türkiye'nin dört bir yanını dolaşarak Linux ve özgür yazılımları anlatan
+seminer konuşmacılarına ise kendi isimleri yazan birer lazer işaretçi
+hediye edildi. Ayrıca şenlik sırasında yapılan oyun turnuvası ve hazine avının
+da ödülleri dağıtıldı.&lt;br /&gt;&lt;br /&gt;
+
+&lt;b&gt;Oyun Turnuvası&lt;/b&gt;:&lt;br /&gt;
+&lt;b&gt;Quake3&lt;/b&gt;&lt;br /&gt;
+1. Sinan Onur Altınuç&lt;br /&gt;
+2. Efe Çiftçi&lt;br /&gt;
+3. Gökhan Şenliler&lt;br /&gt;&lt;br /&gt;
+&lt;b&gt;Unreal Tournament 2004&lt;/b&gt;&lt;br /&gt;
+1. Mert Uzbaşlı&lt;br /&gt;
+2. Erdem Kenar&lt;br /&gt;
+3. Mehmet Gök&lt;br /&gt;&lt;br /&gt;
+&lt;b&gt;Frozen Bubble&lt;/b&gt;&lt;br /&gt;
+1. Gökmen Göksel&lt;br /&gt;
+2. Tuğrul Gürkaynak&lt;br /&gt;
+3. Gökçen Eraslan&lt;br /&gt;&lt;br /&gt;
+
+&lt;b&gt;Hazine Avı&lt;/b&gt;: Çağlar Ateş&lt;br /&gt;&lt;br /&gt;
+
+Son olarak dernek üyeleri arasında yapılan elektronik oylama sonucunda
+belirlenen Yılın Penguenleri Ödülleri sahiplerini buldu :&lt;br /&gt;&lt;br /&gt;
+
+- &lt;b&gt;En Çalışkan Penguen&lt;/b&gt; : &lt;b&gt;&lt;font color=&quot;#ff0000&quot;&gt;Barış Metin&lt;/b&gt;&lt;/font&gt;&lt;br /&gt;
+- &lt;b&gt;En İyi Kurumsal Kullanıcı&lt;/b&gt; : &lt;b&gt;&lt;font color=&quot;#ff0000&quot;&gt;İstanbul Bilgi Üniversitesi Bilgisayar Bilimleri
+Bölümü&lt;/b&gt;&lt;/font&gt;&lt;br /&gt;
+- &lt;b&gt;En Başarılı Yerelleştirici&lt;/b&gt; : &lt;b&gt;&lt;font color=&quot;#ff0000&quot;&gt;Recai Oktaş&lt;/b&gt;&lt;/font&gt;&lt;br /&gt;
+- &lt;b&gt;En İyi Özgür Yazılım&lt;/b&gt; : &lt;b&gt;&lt;font color=&quot;#ff0000&quot;&gt;Zemberek&lt;/b&gt;&lt;/font&gt;&lt;br /&gt;
+- &lt;b&gt;En Başarılı Özgür Yazılım Temelli Uygulama Projesi&lt;/b&gt; : &lt;b&gt;&lt;font color=&quot;#ff0000&quot;&gt;Linuxcuyuz.biz
+Ücretsiz CD Dağıtım Projesi&lt;/b&gt;&lt;/font&gt;&lt;br /&gt;
+- &lt;b&gt;En İyi Basılı/Görsel İçerik Çalışması&lt;/b&gt; : &lt;b&gt;&lt;font color=&quot;#ff0000&quot;&gt;Fazlamesai.net Televizyon
+Programı&lt;/b&gt;&lt;/font&gt;&lt;br /&gt;&lt;br /&gt;
+
+Şenliğin fotoğraf albümünü ve amatör/profesyonel video çekimlerinden
+yapılan derlemeleri yakında şenlik sitesinde bulabileceksiniz.&lt;br /&gt;</description>
+		<pubDate>Tue, 24 May 2005 13:41:11 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenlik devam ediyor</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=75ampItemid=2</link>
+		<description>4. Linux ve Özgür Yazılım Şenliği, birçok önemli konuğun katıldığı açılışın ardından hızla devam ediyor. Şenlikten kısa kısa notlar :&lt;br /&gt;&lt;br /&gt;- Şenliğin ilk günü Afyon Kocatepe Üniversitesi'nden gelen 30 kişilik bir öğrenci grubu şenlik alanına renk kattı. Öğlen arasında 12:30-13:30 saatleri arasında EMO Salonu'nda Afyon'dan gelen öğrencilere özel bir sohbet toplantısı düzenlendi.&lt;br /&gt;&lt;br /&gt;- Kütüphanecilere özgü özgür yazılımların konuşulduğu panele katılım ve ilgi yüksekti. Panelde konuşmaya doyamayan topluluk, ancak Milli Kütüphane salon görevlilerinin ışıkları açıp kapatmaları sonrasında salondan ayrıldı.&lt;br /&gt;&lt;br /&gt;- Şenlik afişini hazırlayan Penguen Dergisi karikatüristi Selçuk Erdem'in kendi çalışmalarında GIMP kullanımını anlattığı oturum seyirci rekoru kırdı.&lt;br /&gt;&lt;br /&gt;- Getirin Bilgisayarınızı Linux Kuralım masasını Sinan Alyürük başarıyla götürüyor. Çoğunluğu dizüstü olmak üzere şu ana kadar 9 bilgisayara Linux kuruldu.&lt;br /&gt;&lt;br /&gt;- Yağmur sesinden kaçan Penguence e-dergisinin yazarları ve okuyucuları, şenlik fuayesinde sohbet toplantılarına devam etti.&lt;br /&gt;&lt;br /&gt;- Şenliğe gelen penguen olarak ayrılıyor. Şenliğin ilk üç gününde derneğe 90 civarı yeni üye katıldı. Şenlik sonuna kadar bu sayının 100'ü aşması bekleniyor.&lt;br /&gt;&lt;br /&gt;- Uzun zamandır beklenen dernek üye kartları basılarak şenlik alanına vardı.&lt;br /&gt;&lt;br /&gt;- &quot;En&quot; penguenlere müjde! Az sayıda da olsa XXXL penguen tişörtleri alana geldi. Kaçırmayın.&lt;br /&gt;&lt;br /&gt;- Yarın şenliğin son günü. Yılın en büyük Linux ve özgür yazılım olayının bir parçası olmak için hala çok geç değil.</description>
+		<pubDate>Sat, 21 May 2005 17:41:40 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenlik başladı!</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=74ampItemid=2</link>
+		<description>4. Linux ve Özgür Yazılım Şenliği görkemli bir açılış töreni ile başladı.
+Linux Kullanıcıları Derneği başkanı Mustafa Akgül'ün açılış konuşmasının
+ardından; sırasıyla Milli Kütüphane Başkanı Tuncel Acar, Elektrik
+Mühendisleri Odası Başkanı Kemal B. Ulusaler, Türkiye Bilişim Derneği
+Başkanı Turhan Menteş, CHP Adana Milletvekili Tacider Seyhan, AKP Denizli
+Milletvekili Mehmet Yüksektepe  Linux'un ve özgür yazılımların ülkemiz için önemini vurgulayan konuşmalar yaptılar. Şenliğin ilk gününe 1000'e yakın ziyaretçi katıldı.</description>
+		<pubDate>Fri, 20 May 2005 14:09:12 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenliğin 0. gününden görüntüler</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=73ampItemid=2</link>
+		<description>4. Linux ve Özgür Yazılım Şenliği'nin 0. günü -- yani kurulum günü dolu dolu geçti. Sabah 9:30'da Milli Kütüphane'de başlayan çalışmalar ancak 21:30'a doğru sonlandı (mı acaba?).
+
+Fotoğraf albümü için &lt;a href=http://senlik.linux.org.tr/2005b/album/senlik0/&gt;tıklayın&lt;/a&gt;.</description>
+		<pubDate>Thu, 19 May 2005 01:34:59 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenlik Haberleri Medya'da</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=72ampItemid=2</link>
+		<description>Şenlik basın bülteni'nin basın kuruluşlarına gönderilmesi ardından bültenimiz haber sütunları arasında yerini aldı.&lt;br /&gt;&lt;br /&gt;
+Anadolu Ajansı - &lt;a href=&quot;http://www.aa.com.tr/?option=com_haber&amp;Itemid=109&quot; target=&quot;_blank&quot;&gt;http://www.aa.com.tr&lt;/a&gt;&lt;br /&gt;
+NTV - &lt;a href=&quot;http://www.ntvmsnbc.com/news/324236.asp&quot; target=&quot;_blank&quot;&gt;http://www.ntvmsnbc.com&lt;/a&gt;&lt;br /&gt;
+CNN Türk - &lt;a href=&quot;http://www.cnnturk.com.tr/BILIM_TEKNOLOJI/BILIM/haber_detay.asp?PID=16&amp;HID=1&amp;haberID=96429&quot; target=&quot;_blank&quot;&gt;http://www.cnnturk.com.tr&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;
+Geçen hafta ise Etkinlik Çalışma Grubu üyesi Ali Erdinç Köroğlu ART Avrasya TV'de yayınlanan WebTV programına konuk olarak şenlik ve özgür yazılım hakkında konuştu. Bunun yanısıra 18 Mayıs 2005 günü Başkanımız Sayın Mustafa Akgül 12.40'ta SkyTV'ye telefonla bağlanarak şenlikle ilgili açıklamalarda bulunacak ve 19 Mayıs 2005 günü saat 13.10'da da Ali Erdinç Köroğlu TRT Radyo1'deki Stüdyo Ankara programına konuk olup şenlikle ilgili konuşacaktır.&lt;br /&gt;&lt;br /&gt;
+Ayrıca Şenlik sponsorumuz KanalB televizyonu da şenlik alanından canlı yayın yaparak IV. Linux ve Özgür Yazılım şenliğini tüm Türkiye'ye aktaracak.&lt;br /&gt;&lt;br /&gt;
+KanalB Turksat 1C ve 2A - Frekans:11.867   Polarite:V   Symbolrate:4.444  Fec:5/6&lt;br /&gt;
+KanalB Digiturk 32. kanalda</description>
+		<pubDate>Tue, 17 May 2005 22:17:06 GMT</pubDate>
+	</item>
+	<item>
+		<title>Haydi gönüllüler şenlik kurulumuna</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=71ampItemid=2</link>
+		<description>İşte o gün geldi çattı..&lt;br /&gt;
+IV. Linux ve Özgür Yazılım Şenliği öncesi kurulum için 18 Mayıs 2005 saat 09.30'da Milli Kütüphane'de buluşacağız. Her zamankinden daha fazla gönüllü yardımına ihtiyaç duyacağımız kurulum gününde bizleri yalnız bırakmayacağınız umar ve tüm gönüllülerimize şimdiden en içten teşekkürlerimizi sunarız..&lt;br /&gt;&lt;br /&gt;
+Yarın Milli Kütüphane'de görüşmek üzere</description>
+		<pubDate>Tue, 17 May 2005 21:35:30 GMT</pubDate>
+	</item>
+	<item>
+		<title>IV. Linux ve Özgür Yazılım Şenliği Başlıyor...</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=69ampItemid=2</link>
+		<description>Linux Kullanıcıları Derneği'nin her yıl düzenlediği Linux ve Özgür Yazılım
+Şenliği 19-22 Mayıs 2005 tarihleri arasında Ankara Milli Kütüphane
+Konferans Salonları'nda gerçekleşecek.
+&lt;br /&gt;&lt;br /&gt;
+Linux ve Özgür Yazılım Şenliği; 16-19 Mayıs 2002'de Ankara Üniversitesi, 
+1-4 Mayıs 2003'te Yıldız Teknik Üniversitesi ve 13-16 Mayıs 2004'te Milli
+Kütüphane'de olduğu gibi bu yıl da Türkiye'deki tüm penguenlerin buluşma ve
+kaynaşmasını sağlayan en büyük etkinlik olma özelliğini taşıyor. Bu yıl
+dördüncüsü düzenlecek olan Linux ve Özgür Yazılım Şenliği, Linux
+dünyasındaki son gelişmeleri yansıtan, hiç Linux bilmeyen ya da az bilen
+kullanıcının, ev kullanıcısının, iş dünyasının ve Linux'a destek veren
+firmaların buluştuğu bir ortam olmayı hedefleyen bir çizgiye sahip.
+&lt;br /&gt;&lt;br /&gt;
+Şenliğin açılışında, Linux Kullanıcıları Derneği Başkanı Sayın Doç. Dr.
+Mustafa Akgül konuşma yapacak.
+&lt;br /&gt;&lt;br /&gt;
+Şenliğin sponsorları IBM Türk Ltd., Prosoft İletişim Sistemleri Ltd. Şti.
+ve 2U Bilişim Teknolojileri olurken; Intel ve Elektrik Mühendisleri Odası
+Ankara Şubesi de etkinliği kurumsal olarak destekliyor. Kayıt masası
+sponsorluğu Parkyeri A.Ş., basın sponsorluğu PCWorld, RadyoODTÜ, KanalB,
+oyun turnuvasının ödülleri ve internet kafede kullanılan bilgisayarları
+Mavi Bilgisayar, kalem sponsorluğu PostgreSQL, içecek sponsorluğu Nescafe
+tarafından üstlenildi.
+&lt;br /&gt;&lt;br /&gt;
+Şenlikte çeşitli konularda 3 paralel salonda yapılacak teknik seminerlerin
+yanısıra, &quot;Yılın Penguenleri Ödülleri&quot;, ayaküstü sohbetler, farklı Linux
+dağıtımlarının kurulumları, oyun turnuvaları, &quot;Getirin Bilgisayarınıza
+Linux Kuralım&quot; kampanyası, internet kafede Linux'la tanışabilme, katılan
+firmalarin yetkilileriyle birebir görüşme fırsatları ve daha birçok
+etkinlik ziyaretçilere sunulacak.
+&lt;br /&gt;&lt;br /&gt;
+Şenliğimize katılım ücretsiz ve herkese açıktır. Gelişmeleri takip etmek ve
+detaylı bilgi edinmek için şenlik web sitesini (&lt;a href=&quot;http://senlik.linux.org.tr&quot; target=&quot;_blank&quot;&gt;http://senlik.linux.org.tr&lt;/a&gt;)
+inceleyebilirsiniz.
+&lt;br /&gt;&lt;br /&gt;
+Yüzlerce gönüllü penguen sayesinde bu yıl dördüncüsünü gerçeklestireceğimiz
+Linux ve Özgür Yazılım Şenliği'ne katılımlarınızla bizlere manevi destek
+vermiş olacaksınız.</description>
+		<pubDate>Tue, 17 May 2005 10:13:14 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenlik öncesi güncelleme</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=68ampItemid=2</link>
+		<description>Şenlik öncesi &lt;a href=&quot;http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=27&amp;Itemid=42&quot; target=&quot;_blank&quot;&gt;Konaklama&lt;/a&gt; ve &lt;a href=&quot;http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=12&amp;Itemid=26&quot; target=&quot;_blank&quot;&gt;Şenlik Programı&lt;/a&gt; güncellendi. Şehir dışından Ankara'ya gelecek ziyaretçilerimizin güncellenen konaklama bilgileri kontrol etmelerini öneririz.</description>
+		<pubDate>Mon, 16 May 2005 13:58:40 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenlik öncesi son LKD Gönüllüleri toplantısı</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=67ampItemid=2</link>
+		<description>Bildiğiniz gibi bu aralar Linux ve Özgür Yazılım Şenliği için çalışmalarımızda 
+depara kalktık artık (&lt;a href=&quot;http://senlik.linux.org.tr&quot; target=&quot;_blank&quot;&gt;http://senlik.linux.org.tr&lt;/a&gt;).&lt;br /&gt;&lt;br /&gt;
+
+Şenlikten önce son bir toplantı yaparak şenlik ile ilgili eksikleri 
+gedikleri masaya yatırıp tüm gönüllüler olarak hep bir elden neler 
+yapabiliriz diye konuşalım istiyoruz.&lt;br /&gt;&lt;br /&gt;
+
+Bu amaçla, 15 Mayıs 2005 Pazar günü 14:00'da Ankara Milli Kütüphane'nin 
+[1] bahçesinde buluşmak istiyoruz.&lt;br /&gt;&lt;br /&gt;
+
+Şenlik için (öncesinde ya da sırasında) bir şeyler yapmak isteyen herkesi 
+bekliyoruz. Bizlere katılırsanız çok seviniriz. Linux'tan anlayın ya da 
+anlamayın farketmez, şenlikle ilgili bir yardımınız muhakkak dokunacaktır.
+&lt;br /&gt;&lt;br /&gt;
+Not : Daha önce Ankara Üniversitesi'nde buluşmayi planlıyorduk ancak 
+üniversitedeki şenlikler yüzünden girişte kimlik kontrolü ve öğrenci 
+olmayanların alınmaması gibi bir durum varmış. O yüzden buluşma yerimizi 
+Milli Kütüphane olarak değiştirdik.
+&lt;br /&gt;&lt;br /&gt;
+[1]  Ankara Milli Kütüphane - Bahçelievler (Akdeniz Caddesi tarafındaki 
+girişte) &lt;a href=&quot;http://senlik.linux.org.tr/2005b/templates/lkd_senlik2005/images/mk-kroki.jpg&quot; target=&quot;_blank&quot;&gt;kroki için tıklayın&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;http://etkinlik.linux.org.tr/senlik-2005-belgeler/senlik2005.pdf&quot; target=&quot;_blank&quot;&gt;Şenlik ile ilgili organizasyon şeması için tıklayın&lt;/a&gt;</description>
+		<pubDate>Sat, 14 May 2005 16:55:19 GMT</pubDate>
+	</item>
+	<item>
+		<title>Şenlik Afişleri / İzmir</title>
+		<link>http://senlik.linux.org.tr/2005b/index.php?option=com_content&amp;task=view&amp;id=65ampItemid=2</link>
+		<description>İzmir'deki afiş dağıtım merkezine henüz afiş ulaşmadığı için dünkü haberimizde duyurmadık. 12 Mayıs perşembe öğleden sonra afişler aşağıdaki adreste olacaklar.&lt;br /&gt;&lt;br /&gt;
+
+&lt;b&gt;İzmir afiş dağıtım yeri :&lt;/b&gt; &lt;br /&gt;
+&lt;u&gt;Alsancak:&lt;/u&gt; &lt;i&gt;Kompusel Mühendislik Bilgisayar Donanım ve Yazılım San. Tic. Ltd. Şti.&lt;/i&gt;  
+  Talatpaşa Bulvarı, Palandöken Apt. No:66/1   (11:00-18:00 
+saatleri arasında)&lt;br /&gt;&lt;br /&gt;
+
+&lt;p align=right&gt;11 Mayıs 2005&lt;/p&gt;</description>
+		<pubDate>Wed, 11 May 2005 12:26:29 GMT</pubDate>
+	</item>
+</channel>
+</rss>

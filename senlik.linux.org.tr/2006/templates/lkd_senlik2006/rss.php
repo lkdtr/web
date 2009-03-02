f45d6a1a5448adffc2f05fe4f1c2fe6516534526
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2009-03-02 12:22:11 +0000

    2006 -- geriye kaldi 1

---
+++ senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php
@@ -0,0 +1,215 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title>V. Linux ve Özgür Yazılım Şenliği</title>
+	<link>http://senlik.linux.org.tr/2006</link>
+	<description>V. Linux and Open Source Festival Site Syndication</description>
+	<managingEditor>Ali Erdinç Köroğlu - erdinc at erdinc.info</managingEditor>
+	<language>tr-TR</language>
+	<generator>AEK RSS Generator 1.0.2</generator>
+	<item>
+		<title>Seminer Videoları</title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=58ampItemid=2</link>
+		<description>&lt;a href=&quot;http://www.bidb.odtu.edu.tr/&quot;&gt;ODTÜ Bilgi İşlem Daire Başkanlığı&lt;/a&gt;'nın katkılarıyla ODTÜ Kültür ve Kongre Merkezi'nde düzenlediğimiz 5. Linux ve Özgür Yazılım Şenliği'nin A Salonu'ndaki seminer videolarına aşağıdaki bağlantılardan indirebilirsiniz;&lt;br /&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Acilis.rm&quot;&gt;Açılış Konuşması&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Beowulf_Kumeleri_ve_Paralel_Programlamaya_Giris-Eray_Ozkural.rm&quot;&gt;Beowulf Kümeleri ve Paralel Programlamaya Giriş - Eray Özkural&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Comar_Yapilandirma_Yoneticisi_Comar_ile_Uygulama_Gelistirme-Bahadir_Kandemir.rm&quot;&gt;Çomar Yapılandırma Yöneticisi Çomar ile Uygulama Geliştirme - Bahadır Kandemir&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Firefox_ile_Web-i_Yeniden_Kesfedin-Arda_Cetin.rm&quot;&gt;Firefox ile Web'i Yeniden Keşfedin - Arda Çetin&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Genel_Pardus_Tanitimi-Baris_Metin.rm&quot;&gt;Genel Pardus Tanıtımı - Barış Metin&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/KDE_4-un_Yenilikleri-Ismail_Donmez-_Baris_Metin.rm&quot;&gt;KDE4'ün Yenilikleri - İsmail Dönmez &amp; Barış Metin&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Lambda_Calculus_Programlama_Dili-Chris_Stephenson.rm&quot;&gt;Lambda Calculus Programlama Dili - Chris Stephenson&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Linux_ve_Masaustu_Dunyasi-Onur_Kucuk.rm&quot;&gt;Linux ve Masaüstü Dünyası - Onur Küçük&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Linux_ve_Ozgur_Hayalgucu-Koray_Loker.rm&quot;&gt;Linux ve Özgür Hayalgücü - Koray Löker&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Pardus_Kurulumu-Ali_Erdinc_Koroglu.rm&quot;&gt;Pardus Kurulumu - Ali Erdinç Köroğlu&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/PiSi_Paket_Yonetim_Sistemi-Eray_Ozkural.rm&quot;&gt;PiSi Paket Yönetim Sistemi - Eray Özkural&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/PiSi_ile_Paket_Yapimi-S-_Caglar_Onur.rm&quot;&gt;PiSi ile Paket Yapımı - S. Çağlar Onur&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/R_Projesi_ile_Uygulamali_Istatistik-Harald_Schmidbauer-_Vehbi_Sinan_Tunalioglu.rm&quot;&gt;R Projesi İle Uygulamalı İstatistik - Harald Schmidbauer &amp; Vehbi Sinan Tunalıoğlu&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Xynth_Pencere_Sistemi-Alper_Akcan-_Caglar_Oral.rm&quot;&gt;Xynth Pencere Sistemi - Alper Akcan / Çağlar Oral&lt;/a&gt;&lt;br /&gt;
+&lt;a href=&quot;ftp://ftp.linux.org.tr/pub/Senlik06/Panel-_Pardus_Gelistiricileri_ile_Kullanicilari_Bulusuyor.rm&quot;&gt;Panel : Pardus Geliştiricileri ile Kullanıcılar Buluşuyor&lt;/a&gt;&lt;br /&gt;</description>
+		<pubDate>Fri, 16 Jun 2006 17:58:09 GMT</pubDate>
+	</item>
+	<item>
+		<title>ODTÜ TV'den A Salonu İstatistikleri</title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=57ampItemid=2</link>
+		<description>Bildiğiniz üzere 5. Linux ve Özgür Yazılım Şenliği'nde A Salonundaki tüm seminerlerimiz &lt;a href=&quot;http://odtutv.metu.edu.tr/&quot;&gt;ODTÜ TV&lt;/a&gt; tarafından canlı olarak yayınlandı.&lt;br /&gt;&lt;br /&gt;
+ODTÜ TV'den gelen izlenme oranlarımız şu şekilde açıklandı;&lt;br /&gt;&lt;br /&gt;
+&lt;TABLE WIDTH=460 BORDER=1 BORDERCOLOR=&quot;#000000&quot; CELLPADDING=7 CELLSPACING=0&gt;
+	&lt;COL WIDTH=100&gt;
+	&lt;COL WIDTH=75&gt;
+	&lt;COL WIDTH=88&gt;
+	&lt;COL WIDTH=88&gt;
+	&lt;COL WIDTH=88&gt;
+	&lt;COL WIDTH=88&gt;
+	&lt;TR VALIGN=TOP&gt;
+		&lt;TD WIDTH=100&gt;
+		&lt;/TD&gt;
+		&lt;TD WIDTH=75&gt;1. Gün&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;2. Gün&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;3. Gün&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;4. Gün&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;Toplam&lt;/TD&gt;
+	&lt;/TR&gt;
+	&lt;TR VALIGN=TOP&gt;
+		&lt;TD WIDTH=100&gt;Toplam&lt;/TD&gt;
+		&lt;TD WIDTH=75&gt;785&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;451&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;57&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;258&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;1551&lt;/TD&gt;
+	&lt;/TR&gt;
+	&lt;TR VALIGN=TOP&gt;
+		&lt;TD WIDTH=100&gt;Modem&lt;/TD&gt;
+		&lt;TD WIDTH=75&gt;52&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;47&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;10&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;26&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;135&lt;/TD&gt;
+	&lt;/TR&gt;
+	&lt;TR VALIGN=TOP&gt;
+		&lt;TD WIDTH=100&gt;ODTU&lt;/TD&gt;
+		&lt;TD WIDTH=75&gt;392&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;206&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;18&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;40&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;656&lt;/TD&gt;
+	&lt;/TR&gt;
+	&lt;TR VALIGN=TOP&gt;
+		&lt;TD WIDTH=100&gt;Kampüs Dışı&lt;/TD&gt;
+		&lt;TD WIDTH=75&gt;341&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;198
+		&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;29&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;192&lt;/TD&gt;
+		&lt;TD WIDTH=88&gt;760&lt;/TD&gt;
+	&lt;/TR&gt;
+&lt;/TABLE&gt;</description>
+		<pubDate>Tue, 06 Jun 2006 19:10:38 GMT</pubDate>
+	</item>
+	<item>
+		<title>Yılın Penguenleri Ödülleri Sahiplerini Buldu!</title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=56ampItemid=2</link>
+		<description>&lt;b&gt;LKD YILIN PENGUENLERI ODULLERI SAHIPLERINI BULDU!&lt;/b&gt;&lt;br /&gt;&lt;br /&gt;
+
+Pazar günü güzel bir ödül töreni ile son bulan 5. Linux ve Özgür
+Yazılım Şenliği'nde, Linux Kullanıcıları Derneği'nin gelenekselleşen
+Yılın Penguenleri Ödülleri de sahiplerini buldu.&lt;br /&gt;&lt;br /&gt;
+
+Dernek üyeleri arasında yapılan oylama sonucunda verilen ödüller şöyle :&lt;br /&gt;
+
+- &lt;b&gt;En Çalışkan Penguen : Necdet Yücel&lt;/b&gt;  (Oy Oranı : %26.43)&lt;br /&gt;
+- &lt;b&gt;En İyi Kurumsal Kullanıcı : Çanakkale Onsekiz Mart Üniversitesi&lt;/b&gt; (Oy Oranı : %35.00)&lt;br /&gt;
+- &lt;b&gt;En Başarılı Özgür Yazılım Projesi : Pardus&lt;/b&gt;  (Oy Oranı : %84.29)&lt;br /&gt;
+- &lt;b&gt;En Başarılı Yerelleştirici : Pınar Yanardağ&lt;/b&gt;   (Oy Oranı : %36.43)&lt;br /&gt;
+- &lt;b&gt;En İyi Özgür Yazılım : PiSi&lt;/b&gt;  (Oy Oranı : %30.71)&lt;br /&gt;
+- &lt;b&gt;En İyi Basılı/Görsel İçerik Çalışması : Belgeler.org&lt;/b&gt;  (Oy Oranı : %54.29)&lt;br /&gt;&lt;br /&gt;
+&lt;em&gt;&lt;b&gt;ONEMLI NOT: Bu yılın En İyi Basılı/Görsel İçerik Çalışması seçilen Belgeler.org bu ödülü redderek LKD Yönetim'ine ödülü geri iade etmiştir.&lt;/b&gt;&lt;/em&gt;</description>
+		<pubDate>Mon, 15 May 2006 10:54:47 GMT</pubDate>
+	</item>
+	<item>
+		<title>Chromium ve Frozen Bubble Kazananları</title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=55ampItemid=2</link>
+		<description>5. Linux ve Özgür Yazılım Şenliği Oyun Turnuvası'nda Chromium B.S.U ve Frozen Bubble kazananları belli oldu.&lt;br /&gt;&lt;br /&gt;
+
+&lt;b&gt;Chromium B.S.U. Kazananları:&lt;/b&gt;&lt;br /&gt;
+1. Doruk Fişek&lt;br /&gt;
+2. Cansin Yıldız&lt;br /&gt;
+3. Atasay Gökkaya&lt;br /&gt;&lt;br /&gt;
+
+&lt;b&gt;Frozen-Bubble: Kazananları:&lt;/b&gt;&lt;br /&gt;
+1. Mert Uzbaşlı&lt;br /&gt;
+2. Faik Uygur&lt;br /&gt;
+3. Oğuz Yarımtepe&lt;br /&gt;&lt;br /&gt;
+
+
+Turnuvamız diğer oyunlarla devam ediyor. Katılmak için hala zamanınız var!</description>
+		<pubDate>Sun, 14 May 2006 12:40:43 GMT</pubDate>
+	</item>
+	<item>
+		<title>&quot;Nesneye Yönelik Programlama&quot; Sunumu İptal Edildi</title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=54ampItemid=2</link>
+		<description>Şenliğin 4. günü 09:30-12:30 arası D Salonu'nda yapılacak olan &quot;Nesneye Yönelik Programlama&quot; semineri, konuşmacının yurtdışında olması sebebiyle iptal edilmiştir.&lt;br /&gt;
+Bilginize.</description>
+		<pubDate>Sat, 13 May 2006 08:50:50 GMT</pubDate>
+	</item>
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 5 on MySQL result index 8 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>24</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 5 on MySQL result index 8 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>25</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 5 on MySQL result index 8 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>26</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 5 on MySQL result index 8 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>27</b><br />
+	<item>
+		<title></title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=ampItemid=2</link>
+		<description></description>
+<br />
+<b>Warning</b>:  mktime() expects parameter 6 to be long, string given in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>38</b><br />
+		<pubDate>Thu, 01 Jan 1970 00:00:00 GMT</pubDate>
+	</item>
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 6 on MySQL result index 9 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>24</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 6 on MySQL result index 9 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>25</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 6 on MySQL result index 9 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>26</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 6 on MySQL result index 9 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>27</b><br />
+	<item>
+		<title></title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=ampItemid=2</link>
+		<description></description>
+<br />
+<b>Warning</b>:  mktime() expects parameter 6 to be long, string given in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>38</b><br />
+		<pubDate>Thu, 01 Jan 1970 00:00:00 GMT</pubDate>
+	</item>
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 7 on MySQL result index 10 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>24</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 7 on MySQL result index 10 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>25</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 7 on MySQL result index 10 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>26</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 7 on MySQL result index 10 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>27</b><br />
+	<item>
+		<title></title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=ampItemid=2</link>
+		<description></description>
+<br />
+<b>Warning</b>:  mktime() expects parameter 6 to be long, string given in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>38</b><br />
+		<pubDate>Thu, 01 Jan 1970 00:00:00 GMT</pubDate>
+	</item>
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 8 on MySQL result index 11 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>24</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 8 on MySQL result index 11 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>25</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 8 on MySQL result index 11 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>26</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 8 on MySQL result index 11 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>27</b><br />
+	<item>
+		<title></title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=ampItemid=2</link>
+		<description></description>
+<br />
+<b>Warning</b>:  mktime() expects parameter 6 to be long, string given in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>38</b><br />
+		<pubDate>Thu, 01 Jan 1970 00:00:00 GMT</pubDate>
+	</item>
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 9 on MySQL result index 12 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>24</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 9 on MySQL result index 12 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>25</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 9 on MySQL result index 12 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>26</b><br />
+<br />
+<b>Warning</b>:  mysql_result() [<a href='function.mysql-result'>function.mysql-result</a>]: Unable to jump to row 9 on MySQL result index 12 in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>27</b><br />
+	<item>
+		<title></title>
+		<link>http://senlik.linux.org.tr/2006/index.php?option=com_content&amp;task=view&amp;id=ampItemid=2</link>
+		<description></description>
+<br />
+<b>Warning</b>:  mktime() expects parameter 6 to be long, string given in <b>/var/www/vhosts/senlik.linux.org.tr/2006/templates/lkd_senlik2006/rss.php</b> on line <b>38</b><br />
+		<pubDate>Thu, 01 Jan 1970 00:00:00 GMT</pubDate>
+	</item>
+</channel>
+</rss>

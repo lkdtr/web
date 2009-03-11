2bd09efb9c392a0b9b4094c9d7d4bf878ad94832
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2009-03-11 20:08:24 +0000

    eski odul phplerine veda

--- odul.linux.org.tr/2006/yonetim/tpl/onayla.aday.tpl.php
+++
@@ -1,9 +0,0 @@
-<!-- start content -->
-  <tr bordercolor="#cccccc">
-    <td bgcolor="#d6dde7">
-    <p></p>
-    <?php onayla_aday_liste(); ?>
-    <p></p>
-    </td>
-  </tr>
-<!-- end content -->
--- odul.linux.org.tr/2008/yonetim/tpl/menu.php
+++
@@ -1,8 +0,0 @@
-<!-- start menu -->
-  <tr>
-    <th scope="col"><?php /*baslik_resmi("1");*/ ?></th>
-  </tr>
-  <tr>
-    <td align="center" bgcolor="#AFBFCF" class="menup"><strong><a href="index.php">Anasayfa</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="onayla.php?tip=yorum">Yorum Yönetimi</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="onayla.php?tip=aday">Aday Yönetimi</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Sonucu Açıkla</a></strong></td>
-  </tr>
-<!-- end menu -->

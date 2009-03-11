a0916269be7692ebc7778b2d25487c2802e3decd
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2009-03-11 20:08:24 +0000

    eski odul phplerine veda

--- odul.linux.org.tr/2005/yorum_ekle.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "./tpl/header.php";
-include "./tpl/yorum_ekle.tpl.php";
-include "./tpl/footer.php";
-?>
--- odul.linux.org.tr/2007/yonetim/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../lib/config.php";
-include "../lib/db.php";
-include "../lib/functions.php";
-include "./tpl/header.php";
-include "./tpl/menu.php";
-include "./tpl/index.tpl.php";
-include "./tpl/footer.php";
-?>

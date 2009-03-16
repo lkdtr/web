9a65c8c7d2cd225aa83a41b9bf9bf49394705b98
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2009-03-16 12:38:17 +0000

    test commiti, bir liste daha nasiplensin arada

--- liste.linux.org.tr/listeler.php
+++ liste.linux.org.tr/listeler.php
@@ -100,6 +100,7 @@
       <br />
       <a class="gir" href="mailto:linux-sunucu-request@liste.linux.org.tr?subject=subscribe">Üye olmak için buraya tıkayın</a>
       <a class="cik" href="mailto:linux-sunucu-request@liste.linux.org.tr?subject=unsubscribe">Listeden çıkmak için buraya tıklayın</a>
+      <a href="http://dir.gmane.org/gmane.org.user-groups.linux.turkey.sunucu">Web/RSS/NNTP ile Okuyun</a>
     </div>
     <hr>
     

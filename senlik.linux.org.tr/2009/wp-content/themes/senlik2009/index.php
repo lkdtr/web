a1c63a987f1eb01edb80b5b8c5ec58c0fd4abb81
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2009-03-31 19:48:06 +0000

    sponsorlarimizi linksiz birakmayalim :)

--- senlik.linux.org.tr/2009/wp-content/themes/senlik2009/index.php
+++ senlik.linux.org.tr/2009/wp-content/themes/senlik2009/index.php
@@ -14,9 +14,11 @@ get_header(); ?>
 
 <h1>Organizasyon</h1>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/istanbulbilgi.png" width="130" />
+<a href="http://www.bilgi.edu.tr/" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/istanbulbilgi.png" width="130" /></a>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/tlkdlogo.png" width="130" />
+<a href="http://www.lkd.org.tr/" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/tlkdlogo.png" width="130" /></a>
+
+<br />
 
 <a href="http://senlik.linux.org.tr/2009/afis-banner/"><img src="http://senlik.linux.org.tr/2009/banner/banner160600-4.jpg" alt="" width="130" /></a>
 
@@ -58,25 +60,25 @@ get_header(); ?>
 
 <h1>Sponsorlar</h1>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/ydn.png" align="right" width="110" />
+<a href="http://developer.yahoo.com/" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/ydn.png" align="right" width="110" /></a>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/pardus.png" align="right" width="110" />
+<a href="http://pardus.org.tr" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/pardus.png" align="right" width="110" /></a>
 
 <h1>İçecek</h1>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/sprite_logo1.jpg" align="right" width="110" />
+<a href="#" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/sprite_logo1.jpg" align="right" width="110" /></a>
 
 <h1>Eşantiyon</h1>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/kartaca-11.png" align="right" width="110" />
+<a href="http://www.kartaca.com/" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/kartaca-11.png" align="right" width="110" /></a>
 
 <h1>Ödül</h1>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/airties.png" align="right" width="110" />
+<a href="http://www.airties.com.tr/" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/airties.png" align="right" width="110" /></a>
 
 <h1>Afiş Tasarım</h1>
 
-<img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/akion.png" align="right" width="80" />
+<a href="http://www.akinon.com/" target="_blank"><img src="http://senlik.linux.org.tr/2009/wp-content/uploads/2009/03/akion.png" align="right" width="80" /></a>
 
 </div>
 

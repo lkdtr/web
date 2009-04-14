9c69e68ae2725888037a3d07a53842932c775317
Melih Taşdizen <melihme@users.noreply.github.com>
2009-04-14 17:10:26 +0000

    script config

--- galeri.linux.org.tr/config.php
+++ galeri.linux.org.tr/config.php
@@ -26,6 +26,7 @@
  * PHP warnings and notices that are not covered in unit tests (e.g. template issues).
  */
 @ini_set('display_errors', 0);
+ini_set("memory_limit","32M");
 
 /*
  * Prevent direct access to config.php.

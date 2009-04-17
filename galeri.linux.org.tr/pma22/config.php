bc06a69ad521d631c2447bd32d23251bcd1d0b92
Melih Taşdizen <melihme@users.noreply.github.com>
2009-04-17 08:25:52 +0000

    pma2 upload

--- galeri.linux.org.tr/pma22/config.php
+++ galeri.linux.org.tr/pma22/config.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'ddddsffffffe4344444444444444444444444444444444'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /*
  * Servers configuration

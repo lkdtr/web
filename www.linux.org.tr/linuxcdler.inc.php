a14320eb537cdf30a4c315744e90c34af92a755a
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2009-05-26 16:16:08 +0000

    e-posta olarak bilgi@ adresi verelim ki, spam'ler tek noktada toplansin

--- www.linux.org.tr/linuxcdler.inc.php
+++ www.linux.org.tr/linuxcdler.inc.php
@@ -2,7 +2,7 @@
 Türkiye'de Linux CD'leri sattığı bilinen web sitelerinin bir listesini aşağıda bulabilirsiniz. 
 Listede yer alan web siteleri LKD tarafından herhangi bir biçimde denetlenmemekte ve içeriği kontrol edilmemektedir. 
 LKD, bu sitelerden yapacağınız alışverişler için herhangi bir biçimde garanti veremez, kefil olamaz. Linux CD'si satan ancak 
-listede bulunmayan siteler için <a href="mailto:web-icerik@linux.org.tr">web-icerik@linux.org.tr</a> 
+listede bulunmayan siteler için <a href="mailto:bilgi@linux.org.tr">bilgi@linux.org.tr</a> 
 adresine e-posta atarak bizi haberdar edebilirsiniz.
 <ul>
  <li type="square">Linux34 (<a target="_blank" href="http://www.linux34.com">www.linux34.com</a>)</li>

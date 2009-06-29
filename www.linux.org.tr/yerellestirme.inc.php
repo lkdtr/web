7a4878d1529cb486a28c866ba6f397dae3d8ceea
Barkın Kılıç <barkin@barkin.info>
2009-06-29 19:34:22 +0000

    yerellestirme link duzeltmeleri

--- www.linux.org.tr/yerellestirme.inc.php
+++ www.linux.org.tr/yerellestirme.inc.php
@@ -15,61 +15,61 @@ Bu sayfada yer almasını düşündüğünüz yerelleştirme faaliyetlerini bild
 <ul>
 <li type="square">
 <strong>Arch Linux:</strong>
-Arch Linux araçlarını Türkçe'ye çeviren Türkiye  topluluğu, aynı zamanda Arch Linux Wiki sayfasında da aktif çeviri çalışmaları yapmaktadır. Çeviriler sürekli güncel tutulmakla birlikte Türkçe'ye çevrilen  doküman sayısı gün geçtikçe artmaktadır.  Yerelleştirme konusunda koordinasyon https://code.archlinux.org.trhttps://code.archlinux.org.tr sitesinde yapılmaktadır.
+Arch Linux araçlarını Türkçe'ye çeviren Türkiye  topluluğu, aynı zamanda Arch Linux Wiki sayfasında da aktif çeviri çalışmaları yapmaktadır. Çeviriler sürekli güncel tutulmakla birlikte Türkçe'ye çevrilen  doküman sayısı gün geçtikçe artmaktadır.  Yerelleştirme konusunda koordinasyon <a href="https://code.archlinux.org.tr">https://code.archlinux.org.tr</a> sitesinde yapılmaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Debian:</strong>
-Debian Türkçe takımının çabalarıyla http://lists.debian.org/debian-l10n-turkish listesi üzerinden Debian'a has araç ve belgelerin Türkçeleştirmesi konusunda faaliyet gösterilmektedir. Ayrıca http://www.debian.org.trhttp://www.debian.org.tr sayfasında da  Türkçe içerik çalışmaları tüm hızıyla devam etmektedir.
+Debian Türkçe takımının çabalarıyla <a href="http://lists.debian.org/debian-l10n-turkish">http://lists.debian.org/debian-l10n-turkish</a> listesi üzerinden Debian'a has araç ve belgelerin Türkçeleştirmesi konusunda faaliyet gösterilmektedir. Ayrıca <a href="http://www.debian.org.tr">http://www.debian.org.tr</a> sayfasında da  Türkçe içerik çalışmaları tüm hızıyla devam etmektedir.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Gentoo:</strong>
-http://www.gentoo-tr.org alan adı altında faaliyet gösteren Gentoo Türkiye ekibi, gerek web siteleri üzerinden, gerekse http://tr.gentoo-wiki.com sayfasında Türkçeleştirme çalışmaları konusunda faaliyet göstermektedir.
+<a href="http://www.gentoo-tr.org">http://www.gentoo-tr.org</a> alan adı altında faaliyet gösteren Gentoo Türkiye ekibi, gerek web siteleri üzerinden, gerekse <a href="http://tr.gentoo-wiki.com">http://tr.gentoo-wiki.com</a> sayfasında Türkçeleştirme çalışmaları konusunda faaliyet göstermektedir.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>GNOME Türkiye:</strong>
-Masaüstü ortamı GNOME, GNOME Türkiye ekibi tarafından Türkçeleştirilmektedir. Türkiye ekibi http://www.gnome.org.tr adresinde faaliyet gösterirken, çeviri koordinasyonu ve haberleşme amacıyla, http://mail.gnome.org/mailman/listinfo/gnome-turk adresinde bir listeye sahip olan grubun tanıtım sayfası ise http://l10n.gnome.org/teams/tr adresinde yer alıyor.
+Masaüstü ortamı GNOME, GNOME Türkiye ekibi tarafından Türkçeleştirilmektedir. Türkiye ekibi <a href="http://www.gnome.org.tr">http://www.gnome.org.tr</a> adresinde faaliyet gösterirken, çeviri koordinasyonu ve haberleşme amacıyla, <a href="http://mail.gnome.org/mailman/listinfo/gnome-turk">http://mail.gnome.org/mailman/listinfo/gnome-turk</a> adresinde bir listeye sahip olan grubun tanıtım sayfası ise <a href="http://l10n.gnome.org/teams/tr">http://l10n.gnome.org/teams/tr</a> adresinde yer alıyor.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>KDE Türkiye:</strong>
-Masaüstü ortamı KDE, KDE Türkiye ekibi tarafından Türkçeye çevrilmektedir. Türkiye ekibinin sayfası http://www.kde.org.tr üzerinde yer alırken, çeviri koordinasyonu ve haberleşme amacıyla, http://www.kde.org.tr/mailman/listinfo/kde ve http://www.kde.org.tr/mailman/listinfo/yerellestirme listelerinde faal çalışmalar sürdürülmektedir.
+Masaüstü ortamı KDE, KDE Türkiye ekibi tarafından Türkçeye çevrilmektedir. Türkiye ekibinin sayfası <a href="http://www.kde.org.tr">http://www.kde.org.tr</a> üzerinde yer alırken, çeviri koordinasyonu ve haberleşme amacıyla, <a href="http://www.kde.org.tr/mailman/listinfo/kde">http://www.kde.org.tr/mailman/listinfo/kde</a> ve <a href="http://www.kde.org.tr/mailman/listinfo/yerellestirme">http://www.kde.org.tr/mailman/listinfo/yerellestirme</a> listelerinde faal çalışmalar sürdürülmektedir.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Mandriva:</strong>
-Linux dağıtımı Mandriva için gerek sistem araçları gerekse belgelendirme amacıyla kurulmuş olan Mandriva Türkiye grubu koordinasyonu http://www.mandrivaturkiye.com sitesinde gerçekleştirmektedir.
+Linux dağıtımı Mandriva için gerek sistem araçları gerekse belgelendirme amacıyla kurulmuş olan Mandriva Türkiye grubu koordinasyonu <a href="http://www.mandrivaturkiye.com">http://www.mandrivaturkiye.com</a> sitesinde gerçekleştirmektedir.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Pardus:</strong>
-TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için genel bir yerelleştirme listesi ile birlikte farklı diller için de birer yerelleştirme listesi bulunmaktadır. Pardus araçlarının yerelleştirilmesiyle sorumlu çeviri grubu yerelleştirme koordinasyonu da liste üzerinden gerçekleştirmektedir.  Pardus yerelleştirme ekibi aynı zamanda KDE  ve KDE dokümanlarının Türkçeye çevrilmesi konusunda da çalışmalarını sürdürmektedir . Yerelleştirme çalışmalarına katılmak için http://liste.pardus.org.tr/mailman/listinfo/turkce liste adresinden gereken bilgi alınabilir. 
+TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için genel bir yerelleştirme listesi ile birlikte farklı diller için de birer yerelleştirme listesi bulunmaktadır. Pardus araçlarının yerelleştirilmesiyle sorumlu çeviri grubu yerelleştirme koordinasyonu da liste üzerinden gerçekleştirmektedir.  Pardus yerelleştirme ekibi aynı zamanda KDE  ve KDE dokümanlarının Türkçeye çevrilmesi konusunda da çalışmalarını sürdürmektedir . Yerelleştirme çalışmalarına katılmak için <a href="http://liste.pardus.org.tr/mailman/listinfo/turkce">http://liste.pardus.org.tr/mailman/listinfo/turkce</a> liste adresinden gereken bilgi alınabilir. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Suse:</strong>
-Linux dağıtımı (Open)Suse için sistem araçlarının çevrilmesi ve belgelendirme amacıyla kurulmuş olan http://www.suse-tr.com sitesinden yerelleştirme çalışmalarına katkıda bulunabilirsiniz.
+Linux dağıtımı (Open)Suse için sistem araçlarının çevrilmesi ve belgelendirme amacıyla kurulmuş olan <a href="http://www.suse-tr.com">http://www.suse-tr.com</a> sitesinden yerelleştirme çalışmalarına katkıda bulunabilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Ubuntu:</strong>
-Ubuntu için çalışmalarda bulunan Ubuntu Türkiye ekibi, Ubuntu dağıtımındaki araçların yerelleştirilmesi  ve belgelendirme konusunda faaliyet göstermektedir. Ubuntu Türkiye ekibinin web sitesine http://www.ubuntu-tr.org adresinden ulaşabilirsiniz. Ayrıca https://launchpad.net/~ubuntu-l10n-tr adresinde bulunan “Ubuntu'yu Türkçe'ye Çevirenler” takımı da Ubuntu'nun yerelleştirilmesi konusunda çaba sarf etmektedir.
+Ubuntu için çalışmalarda bulunan Ubuntu Türkiye ekibi, Ubuntu dağıtımındaki araçların yerelleştirilmesi  ve belgelendirme konusunda faaliyet göstermektedir. Ubuntu Türkiye ekibinin web sitesine <a href="http://www.ubuntu-tr.org">http://www.ubuntu-tr.org</a> adresinden ulaşabilirsiniz. Ayrıca <a href="https://launchpad.net/~ubuntu-l10n-tr">https://launchpad.net/~ubuntu-l10n-tr</a> adresinde bulunan “Ubuntu'yu Türkçe'ye Çevirenler” takımı da Ubuntu'nun yerelleştirilmesi konusunda çaba sarf etmektedir.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Xfce Türkiye:</strong>
-Henüz bir web sitesi bulunmayan Xfce Türkiye ekibi, masaüstü ortamı  olan Xfce konusunda çeviri çalışmaları yürütmektedir. Xfce masaüstü yöneticisinin 4.6 sürümünden itibaren %100 çeviri oranını yakalayan ekip, haberleşme ve çeviri koordinasyonunu http://groups.google.com/group/xfce-tr listesi üzerinden gerçekleştirmektedir.
+Henüz bir web sitesi bulunmayan Xfce Türkiye ekibi, masaüstü ortamı  olan Xfce konusunda çeviri çalışmaları yürütmektedir. Xfce masaüstü yöneticisinin 4.6 sürümünden itibaren %100 çeviri oranını yakalayan ekip, haberleşme ve çeviri koordinasyonunu <a href="http://groups.google.com/group/xfce-tr">http://groups.google.com/group/xfce-tr</a> listesi üzerinden gerçekleştirmektedir.
 <br/>
 <hr noshade="" size="1"/>
 </li>

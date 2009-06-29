646af81141a4e9ca8ed71faacc9908da368b6e30
Barkın Kılıç <barkin@barkin.info>
2009-06-29 18:11:04 +0000

    Bozuldu duzeltelim

---
+++ www.linux.org.tr/yerellestirme-yeni.inc.php
@@ -0,0 +1,26 @@
+TÜRKİYE'DE ÖZGÜR YAZILIM YERELLEŞTİRME ÇALIŞMALARI ve EKİPLERİ
+
+Alfabetik sıraya göre düzenlenmiştir:
+
+Arch Linux: Arch Linux araçlarını Türkçe'ye çeviren Türkiye  topluluğu, aynı zamanda Arch Linux Wiki sayfasında da aktif çeviri çalışmaları yapmaktadır. Çeviriler sürekli güncel tutulmakla birlikte Türkçe'ye çevrilen  doküman sayısı gün geçtikçe artmaktadır.  Yerelleştirme konusunda koordinasyon https://code.archlinux.org.tr sitesinde yapılmaktadır.
+
+Debian: Debian Türkçe takımının çabalarıyla http://lists.debian.org/debian-l10n-turkish listesi üzerinden Debian'a has araç ve belgelerin Türkçeleştirmesi konusunda faaliyet gösterilmektedir. Ayrıca http://www.debian.org.tr sayfasında da  Türkçe içerik çalışmaları tüm hızıyla devam etmektedir.   
+
+Gentoo: http://www.gentoo-tr.org alan adı altında faaliyet gösteren Gentoo Türkiye ekibi, gerek web siteleri üzerinden, gerekse http://tr.gentoo-wiki.com sayfasında Türkçeleştirme çalışmaları konusunda faaliyet göstermektedir.
+
+GNOME Türkiye: Masaüstü ortamı GNOME, GNOME Türkiye ekibi tarafından Türkçeleştirilmektedir. Türkiye ekibi http://www.gnome.org.tr adresinde faaliyet gösterirken, çeviri koordinasyonu ve haberleşme amacıyla, http://mail.gnome.org/mailman/listinfo/gnome-turk adresinde bir listeye sahip olan grubun tanıtım sayfası ise http://l10n.gnome.org/teams/tr adresinde yer alıyor.
+
+KDE Türkiye: Masaüstü ortamı KDE, KDE Türkiye ekibi tarafından Türkçeye çevrilmektedir. Türkiye ekibinin sayfası http://www.kde.org.tr üzerinde yer alırken, çeviri koordinasyonu ve haberleşme amacıyla, http://www.kde.org.tr/mailman/listinfo/kde ve http://www.kde.org.tr/mailman/listinfo/yerellestirme listelerinde faal çalışmalar sürdürülmektedir. 
+
+Mandriva: Linux dağıtımı Mandriva için gerek sistem araçları gerekse belgelendirme amacıyla kurulmuş olan Mandriva Türkiye grubu koordinasyonu http://www.mandrivaturkiye.com sitesinde gerçekleştirmektedir.
+
+Pardus: TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için genel bir yerelleştirme listesi ile birlikte farklı diller için de birer yerelleştirme listesi bulunmaktadır. Pardus araçlarının yerelleştirilmesiyle sorumlu çeviri grubu yerelleştirme koordinasyonu da liste üzerinden gerçekleştirmektedir.  Pardus yerelleştirme ekibi aynı zamanda KDE  ve KDE dokümanlarının Türkçeye çevrilmesi konusunda da çalışmalarını sürdürmektedir . Yerelleştirme çalışmalarına katılmak için http://liste.pardus.org.tr/mailman/listinfo/turkce liste adresinden gereken bilgi alınabilir. 
+
+Suse: Linux dağıtımı (Open)Suse için sistem araçlarının çevrilmesi ve belgelendirme amacıyla kurulmuş olan http://www.suse-tr.com sitesinden yerelleştirme çalışmalarına katkıda bulunabilirsiniz.
+
+Ubuntu: Ubuntu için çalışmalarda bulunan Ubuntu Türkiye ekibi, Ubuntu dağıtımındaki araçların yerelleştirilmesi  ve belgelendirme konusunda faaliyet göstermektedir. Ubuntu Türkiye ekibinin web sitesine http://www.ubuntu-tr.org adresinden ulaşabilirsiniz. Ayrıca https://launchpad.net/~ubuntu-l10n-tr adresinde bulunan “Ubuntu'yu Türkçe'ye Çevirenler” takımı da Ubuntu'nun yerelleştirilmesi konusunda çaba sarf etmektedir.   
+
+Xfce Türkiye: Henüz bir web sitesi bulunmayan Xfce Türkiye ekibi, masaüstü ortamı  olan Xfce konusunda çeviri çalışmaları yürütmektedir. Xfce masaüstü yöneticisinin 4.6 sürümünden itibaren %100 çeviri oranını yakalayan ekip, haberleşme ve çeviri koordinasyonunu http://groups.google.com/group/xfce-tr listesi üzerinden gerçekleştirmektedir.
+
+
+Bu sayfada yer almasını düşündüğünüz yerelleştirme faaliyetlerini bildirmek veya hatalı olduğunu düşündüğünüz bilgileri iletmek için bilgi@linux.org.tr adresi ile iletişime geçebilirsiniz.

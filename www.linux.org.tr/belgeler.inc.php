7bfe5be4dc727f3c138efb736ff36977179450a7
Samed Beyribey <ras0ir@eventualis.org>
2009-07-05 19:47:06 +0000

    belgeler sayfasindaki linkler duzenlendi

--- www.linux.org.tr/belgeler.inc.php
+++ www.linux.org.tr/belgeler.inc.php
@@ -3,7 +3,8 @@
 <h1>Linux Nasıl Belgeleri ve Diğer Belgeler</h1>
 Geçtiğimiz yıllarda bu sayfada, ortak belge havuzu niteliği taşıyan belgeler.org üzerinde durulurken, günümüzde GNU/Linux dağıtımlarının ve diğer işletim sistemlerinin ülkemizde yaygınlık kazanması, dağıtımlara has kullanıcı grupları oluşması ve bu kullanıcı gruplarının çalışmaları sonucunda, çeşitli konumlarda bir çok doküman havuzu oluşturulmaya başlanmıştır. Bu sayfada, Türkiye'de GNU/Linux, diğer özgür işletim sistemleri ve çeşitli programlama dilleri hakkında çalışmaların yapıldığı doküman havuzlarının tanıtımı yapılmaktadır.
 <br/>
-Alfabetik sıraya göre düzenlenmiştir.
+<br/>
+<em>Alfabetik sıraya göre düzenlenmiştir.</em>
 <br/>
 <span class="box" style="width: 20%;">
 Bu sayfada yer almasını düşündüğünüz belgelendirme faaliyetlerini bildirmek veya hatalı olduğunu düşündüğünüz bilgileri iletmek için <a href="mailto:bilgi@linux.org.tr">bizimle</a> bağlantıya geçebilirsiniz.
@@ -11,115 +12,115 @@ Bu sayfada yer almasını düşündüğünüz belgelendirme faaliyetlerini bildi
 <ul>
 <li type="square">
 <strong>Apache Web Sunucusu Belgeleri:</strong>
-Apache web sunucusunun 2.0, 2.2 ve 2.3 serileri için dokümanlarını barındıran belgelendirme çalışmaları sitesine http://ceviri.belgeler.gen.tr/apache/ adresinden ulaşabilir ve katkıda bulunabilirsiniz. Yapılan çalışmalar http://httpd.apache.org/docs/ sayfasına yansımaktadır.
+Apache web sunucusunun 2.0, 2.2 ve 2.3 serileri için dokümanlarını barındıran belgelendirme çalışmaları sitesine <a href="http://ceviri.belgeler.gen.tr/apache/">http://ceviri.belgeler.gen.tr/apache/</a> adresinden ulaşabilir ve katkıda bulunabilirsiniz. Yapılan çalışmalar http://httpd.apache.org/docs/ sayfasına yansımaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Arch Linux Wiki:</strong>
-Arch Linux hakkında belgelendirme çalışmaları http://wiki.archlinux.org üzerinde yapılmaktadır.  Türkçe dokümanların tamamına http://wiki.archlinux.org/index.php/Category:Türkçe adresinden erişilebilir.
+Arch Linux hakkında belgelendirme çalışmaları <a href="http://wiki.archlinux.org">http://wiki.archlinux.org</a> üzerinde yapılmaktadır.  Türkçe dokümanların tamamına <a href="http://wiki.archlinux.org/index.php/Category:Türkçe">bu adresten</a> erişilebilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Belgeler.org:</strong>
-Belgelendirme çalışmaları konusunda ülkemizde bir ilki gerçekleştirerek çeşitli konularda yazılmış oldukça fazla sayıda NASIL (HOWTO) belgesini biraraya getirerek doküman havuzu niteliği kazanan belgeler.org sitesi çoğu işlemde halâ ilk referans niteliği taşıyan belgeleri bünyesinde barındırmaktadır.  Belgeler.org bünyesinde NASIL belgelerinin yanı sıra, PHP dili ve Apache web sunucusu dokümanların çevirileri yapılmaktadır.
+Belgelendirme çalışmaları konusunda ülkemizde bir ilki gerçekleştirerek çeşitli konularda yazılmış oldukça fazla sayıda NASIL (HOWTO) belgesini biraraya getirerek doküman havuzu niteliği kazanan belgeler.org sitesi çoğu işlemde halâ ilk referans niteliği taşıyan belgeleri bünyesinde barındırmaktadır. <a href="http://www.belgeler.org">Belgeler.org</a> bünyesinde NASIL belgelerinin yanı sıra, PHP dili ve Apache web sunucusu dokümanların çevirileri yapılmaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>COMU:</strong>
-Çanakkale On Sekiz Mart Üniversitesi Linux Belgelendirme Çalışma Grubu tarafından hazırlanan http://docs.comu.edu.tr/ sayfasında güncel NASIL belgeleri ve çeşitli kılavuzlara erişim mümkün olmaktadır. 
+Çanakkale On Sekiz Mart Üniversitesi Linux Belgelendirme Çalışma Grubu tarafından hazırlanan <a href="http://docs.comu.edu.tr/">http://docs.comu.edu.tr</a> sayfasında güncel NASIL belgeleri ve çeşitli kılavuzlara erişim mümkün olmaktadır. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Debian:</strong>
-Yeni yapılanmasının ardından faaliyete geçen Debian Türkiye topluluğu, http://www.debian.org.tr adresinde Debian konusunda Türkçe belgelendirme çalışmaları yapmaktadır. 
+Yeni yapılanmasının ardından faaliyete geçen Debian Türkiye topluluğu, <a href="http://www.debian.org.tr">http://www.debian.org.tr</a> adresinde Debian konusunda Türkçe belgelendirme çalışmaları yapmaktadır. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>EnderUNIX:</strong>
-FreeBSD ve diğer BSD temelli işletim sistemleri konusunda belgelendirme çalışmaları yapılan Enderunix web sitesinde *BSD işletim sistemleri konusunda kapsamlı belgeler bulunmaktadır. http://www.enderunix.org/?lng=tr&page=papers adresinden ulaşılabilecek belgeler ve http://ipucu.enderunix.org sayfasında yer alan ipucu belgeleri ile *BSD konusunda kapsamlı bir doküman havuzu oluşturulmaktadır.
+FreeBSD ve diğer BSD temelli işletim sistemleri konusunda belgelendirme çalışmaları yapılan Enderunix web sitesinde *BSD işletim sistemleri konusunda kapsamlı belgeler bulunmaktadır. <a href="http://www.enderunix.org/?lng=tr&page=papers">Bu adresten</a> ulaşılabilecek belgeler ve <a href="http://ipucu.enderunix.org">http://ipucu.enderunix.org</a> sayfasında yer alan ipucu belgeleri ile *BSD konusunda kapsamlı bir doküman havuzu oluşturulmaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Gentoo Belgeleri:</strong>
-Gentoo Linux hakkında çeşitli belgeler, http://www.gentoo-tr.com ve http://www.gentoo-tr.org sitelerinde yer almaktadır. Ayrıca, http://tr.gentoo-wiki.com altında ortak bir doküman havuzu oluşturma konusunda faaliyetler devam etmektedir. 
+Gentoo Linux hakkında çeşitli belgeler, <a href="http://www.gentoo-tr.com">http://www.gentoo-tr.com</a> ve <a href="http://www.gentoo-tr.org">http://www.gentoo-tr.org</a> sitelerinde yer almaktadır. Ayrıca, <a href="http://tr.gentoo-wiki.com">Gentoo Wiki</a> altında ortak bir doküman havuzu oluşturma konusunda faaliyetler devam etmektedir. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Lapis Wiki:</strong>
-Gönüllüler tarafından oluşturulmuş, çeşitli Linux dağıtımları hakkında Türkçeleştirilmiş NASIL belgeleri, ipuçları ve benzeri belgeleri kapsayan Lapis Wiki'ye http://wiki.linux-sevenler.org adresinden ulaşabilirsiniz. 
+Gönüllüler tarafından oluşturulmuş, çeşitli Linux dağıtımları hakkında Türkçeleştirilmiş NASIL belgeleri, ipuçları ve benzeri belgeleri kapsayan Lapis Wiki'ye <a href="http://wiki.linux-sevenler.org">http://wiki.linux-sevenler.org</a> adresinden ulaşabilirsiniz. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Linux İpuçları:</strong>
-Yeni başlayanların Linux ile ilgili temel bilgilere erişebileceği, ileri seviyede Linux bilgisine sahip kişilerin bilgilerini paylaşabilecekleri bir platform olarak kendini tanıtan http://www.linuxipuclari.com sitesi, çeşitli dağıtımlara ilişkin bir ipucu havuzu niteliği taşımaktadır.
+Yeni başlayanların Linux ile ilgili temel bilgilere erişebileceği, ileri seviyede Linux bilgisine sahip kişilerin bilgilerini paylaşabilecekleri bir platform olarak kendini tanıtan <a href="http://www.linuxipuclari.com">Linux İpuçları</a> sitesi, çeşitli dağıtımlara ilişkin bir ipucu havuzu niteliği taşımaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>OpenOffice.org Belgeleri (Gazi Üniversitesi):</strong>
-Gazi Üniversitesi Bilgi İşlem Dairesi Başkanlığı tarafından oluşturulmuş, OpenOffice bileşenleri konusunda kapsamlı bir anlatım içeren OpenOffice Kullanım Kılavuzuna http://www.openoffice.gazi.edu.tr adresinden erişilebilirsiniz.
+Gazi Üniversitesi Bilgi İşlem Dairesi Başkanlığı tarafından oluşturulmuş, OpenOffice bileşenleri konusunda kapsamlı bir anlatım içeren OpenOffice Kullanım Kılavuzuna <a href="http://www.openoffice.gazi.edu.tr">http://www.openoffice.gazi.edu.tr</a> adresinden erişilebilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>OpenOffice.org Türkiye Wiki:</strong>
-OpenOffice Türkiye ekibi tarafından belgelendirme çalışmalarını yürütmek ve OpenOffice kullanımını anlatmak amacıyla hayata geçirilmiş olan OpenOffice Wiki'ye http://wiki.openoffice.org.tr adresinden ulaşabilirsiniz.
+OpenOffice Türkiye ekibi tarafından belgelendirme çalışmalarını yürütmek ve OpenOffice kullanımını anlatmak amacıyla hayata geçirilmiş olan OpenOffice Wiki'ye <a href="http://wiki.openoffice.org.tr">http://wiki.openoffice.org.tr</a> adresinden ulaşabilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Özgürlükİçin:</strong>
-TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için topluluk sitesi vasfını taşıyan Özgürlükİçin sitesinde, Pardus ve dolayısıyla Linux ile ilk defa tanışacak kişiler için İlk Adımlar sayfaları, çeşitli paket incelemelerinin yapıldığı ve uygulamaların tanıtıldığı Nasıl bölümü, GNU/Linux oyunları hakkında belgeler vb. nitelikte belgelendirme çalışmaları yürütülmektedir. Özgürlükİçin sitesine, http://www.ozgurlukicin.com adresinden ulaşabilirsiniz.
+TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için topluluk sitesi vasfını taşıyan Özgürlükİçin sitesinde, Pardus ve dolayısıyla Linux ile ilk defa tanışacak kişiler için İlk Adımlar sayfaları, çeşitli paket incelemelerinin yapıldığı ve uygulamaların tanıtıldığı Nasıl bölümü, GNU/Linux oyunları hakkında belgeler vb. nitelikte belgelendirme çalışmaları yürütülmektedir. Özgürlükİçin sitesine, <a href="http://www.ozgurlukicin.com">http://www.ozgurlukicin.com</a> adresinden ulaşabilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Pardus Wiki:</strong>
- TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için doküman havuzu niteliğini gören Pardus Wiki'de, Pardus konusunda oldukça fazla sayıda doküman, geliştirici kılavuzları,  Pardus hakkında teknik kılavuzlar vb. belge bulunmaktadır. Pardus Wiki'ye http://tr.pardus-wiki.org sitesinden ulaşabilirsiniz. 
+ TÜBİTAK/UEKAE bünyesinde geliştirilen Pardus dağıtımı için doküman havuzu niteliğini gören Pardus Wiki'de, Pardus konusunda oldukça fazla sayıda doküman, geliştirici kılavuzları,  Pardus hakkında teknik kılavuzlar vb. belge bulunmaktadır. Pardus Wiki'ye <a href="http://tr.pardus-wiki.org">http://tr.pardus-wiki.org</a> sitesinden ulaşabilirsiniz. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Pardus-Linux.org Wiki:</strong>
-http://wiki.pardus-linux.org adresinde faaliyet gösteren sitede, Pardus ağırlıklı GNU/Linux belgelendirme çalışmaları yürütülmektedir. Diğer Pardus belgelendirme sitelerinde olduğu gibi, bu doküman havuzunda da, NASIL belgeleri ve çeşitli belgeler yer almaktadır.
+<a href="http://wiki.pardus-linux.org">http://wiki.pardus-linux.org</a> adresinde faaliyet gösteren sitede, Pardus ağırlıklı GNU/Linux belgelendirme çalışmaları yürütülmektedir. Diğer Pardus belgelendirme sitelerinde olduğu gibi, bu doküman havuzunda da, NASIL belgeleri ve çeşitli belgeler yer almaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>PHP Kılavuzu:</strong>
- Belgeler.org bünyesinde yerelleştirilen ve Türkçeye kazandırılan PHP kılavuzuna http://www.php.net/manual/tr/ adresinden erişebilirsiniz. Kılavuz bünyesinde, PHP'ye ilişkin tüm referans bilgiler Türkçeleştirilerek Türk kullanıcıların hizmetine sunulmuştur.
+ Belgeler.org bünyesinde yerelleştirilen ve Türkçeye kazandırılan PHP kılavuzuna <a href="http://www.php.net/manual/tr/">http://www.php.net/manual/tr/</a> adresinden erişebilirsiniz. Kılavuz bünyesinde, PHP'ye ilişkin tüm referans bilgiler Türkçeleştirilerek Türk kullanıcıların hizmetine sunulmuştur.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Python Belgeleri:</strong>
-Ülkemizde son yıllarda popülerlik kazanmış Python programlama dili konusunda çeşitli sitelerde Türkçe belgelendirme çalışmaları yapılmaktadır. Http://pythontr.blogspot.com ve http://www.istihza.com adreslerinde bulunan dokümanlar Python konusunda zengin birer içerik havuzu niteliği taşımaktadır.
+Ülkemizde son yıllarda popülerlik kazanmış Python programlama dili konusunda çeşitli sitelerde Türkçe belgelendirme çalışmaları yapılmaktadır. <a href="http://pythontr.blogspot.com">http://pythontr.blogspot.com</a> ve <a href="http://www.istihza.com">http://www.istihza.com</a> adreslerinde bulunan dokümanlar Python konusunda zengin birer içerik havuzu niteliği taşımaktadır.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Qt Türkiye:</strong>
-Qt konusunda Türkçe ve özgün belgeler sunmak ve Qt ile yapılmış projelerin listesini tutup bunları tanıtmak gibi belgelendirme çalışmalarını sürdüren Qt Türkiye'nin kullanıma sunmuş  olduğu belgelere http://qt.comu.edu.tr/belgeler.php adresinden ulaşabilirsiniz.
+Qt konusunda Türkçe ve özgün belgeler sunmak ve Qt ile yapılmış projelerin listesini tutup bunları tanıtmak gibi belgelendirme çalışmalarını sürdüren Qt Türkiye'nin kullanıma sunmuş  olduğu belgelere <a href="http://qt.comu.edu.tr/belgeler.php">bu adresten</a> ulaşabilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Truva Linux Wiki:</strong>
-Amacı Türk kullanıcıların kullanımına uygun kolay kullanılabilir bir dağıtım geliştirmek olan Truva Linux için, http://www.truvalinux.org.tr/wiki/ adresinde kapsamlı bir belge havuzu bulunmaktadır. 
+Amacı Türk kullanıcıların kullanımına uygun kolay kullanılabilir bir dağıtım geliştirmek olan Truva Linux için, <a href="http://www.truvalinux.org.tr/wiki/">http://www.truvalinux.org.tr/wiki/</a> adresinde kapsamlı bir belge havuzu bulunmaktadır. 
 <br/>
 <hr noshade="" size="1"/>
 </li>
 <li type="square">
 <strong>Ubuntu Türkiye Wiki:</strong>
-Ubuntu hakkında Türkçe doküman havuzu hazırlamak üzere çalışmalarda bulunan Ubuntu Türkiye ekibi tarafından hazırlanan Ubuntu Türkçe Wiki'de oldukça fazla sayıda, Ubuntu dağıtımına has belge yer almaktadır.
+Ubuntu hakkında Türkçe doküman havuzu hazırlamak üzere çalışmalarda bulunan Ubuntu Türkiye ekibi tarafından hazırlanan Ubuntu Türkçe Wiki'de oldukça fazla sayıda, Ubuntu dağıtımına has belge yer almaktadır. Ubuntu Türkiye Wiki'ye <a href="http://wiki.ubuntu-tr.org">bu adresten</a> ulaşabilirsiniz.
 <br/>
 <hr noshade="" size="1"/>
 </li>

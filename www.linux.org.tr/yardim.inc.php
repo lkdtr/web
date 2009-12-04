895ffe01cd78331de38bb38b58d78d63bfde500a
Barkın Kılıç <barkin@barkin.info>
2009-12-04 18:21:49 +0000

    son hali

--- www.linux.org.tr/yardim.inc.php
+++ www.linux.org.tr/yardim.inc.php
@@ -2,74 +2,179 @@
 <div style="padding-left: 5px;">
 <h2>YARDIMLAŞMA NOKTALARI</h2>
 Özgür yazılım ekosisteminin bilindiği gibi en temel dayanağı, karşılıksız birbirine yardım etme prensibidir. Ülkemizde de dünyadaki özgür yazılım ekosistemine paralel olarak çeşitli dallarda yardımlaşma noktaları oluşturulmuştur ve her geçen gün bu yardımlaşma noktaları sayısı artmakta ve belirli nitelik kazanmaktadırlar.
-
+<br/>
 Yardımlaşma konusunda en temel ayrım, e-posta listeleri ve forumlar ve IRC (Internet Relay Chat) ve haber grupları olarak göze çarpmaktadır. Aşağıdaki listede GNU/Linux, *BSD işletim sistemleri ve diğer özgür yazılımlara ilişkin yardımlaşma noktalarının e-posta listeleri, forumları ve IRC kanalları ve haber grupları için gereken adresleri bulabilirsiniz. (Listeler alfabetik sıraya göre düzenlenmiştir)
-
+<br/>
 Aşağıda listede yer almayan fakat listede yer almasını düşündüğünüz yardımlaşma noktalarını bilgi@linux.org.tr adresine e-posta atmak suretiyle bize bildirebilirsiniz.
 
 <h2>E-Posta Listeleri</h2>
 
 Yerelleştirmeye ilişkin e-posta listeleri, aşağıda gösterilmemektedir. Yerelleştirme listelerine, Yerelleştirme sayfasından ulaşabilirsiniz.
+<br/>
 
-Arch Linux Kullanıcıları E-posta Listesi: Arch Linux topluluğu tarafından hizmete sunulan e-posta listesinde Arch Linux dağıtımına ilişkin her türlü teknik destek verilmekte ve yardımlaşma yapılmaktadır. Liste hakkında detaylı bilgiye ve liste arşivlerine http://liste.archlinux.org.tr/mailman/listinfo/archlinux adresinden ulaşabilirsiniz.
+<ul>
+<li type="square">
+<strong>Arch Linux Kullanıcıları E-posta Listesi: </strong> Arch Linux topluluğu tarafından hizmete sunulan e-posta listesinde Arch Linux dağıtımına ilişkin her türlü teknik destek verilmekte ve yardımlaşma yapılmaktadır. Liste hakkında detaylı bilgiye ve liste arşivlerine <a href="http://liste.archlinux.org.tr/mailman/listinfo/archlinux" target="_blank">http://liste.archlinux.org.tr/mailman/listinfo/archlinux</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Debian Kullanıcıları E-posta Listesi: Debian kullanıcılarına yönelik hizmet vermekte olan debian-users-turkish listesi, Debian dağıtımına ilişkin her türlü teknik destek ve yardımlaşmanın yapıldığı bir e-posta listesi olarak faaliyet göstermektedir. Liste hakkında detaylı bilgiye ve liste arşivlerine http://lists.debian.org/debian-user-turkish/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Debian Kullanıcıları E-posta Listesi: </strong> Debian kullanıcılarına yönelik hizmet vermekte olan debian-users-turkish listesi, Debian dağıtımına ilişkin her türlü teknik destek ve yardımlaşmanın yapıldığı bir e-posta listesi olarak faaliyet göstermektedir. Liste hakkında detaylı bilgiye ve liste arşivlerine <a href="http://lists.debian.org/debian-user-turkish/" target="_blank">http://lists.debian.org/debian-user-turkish/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Enderunix E-posta Listeleri: BSD tabanlı işletim sistemleri, güvenlik ve çeşitli özgür yazılımlara ilişkin listeleri bünyesinde barındıran Enderunix listeleri hakkında, listelere katılım ve arşiv gibi bilgilere http://www.enderunix.org/?lng=tr&page=services#maillists adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Enderunix E-posta Listeleri: </strong> BSD tabanlı işletim sistemleri, güvenlik ve çeşitli özgür yazılımlara ilişkin listeleri bünyesinde barındıran Enderunix listeleri hakkında, listelere katılım ve arşiv gibi bilgilere <a href="http://www.enderunix.org/?lng=tr&page=services#maillists" target="_blank">http://www.enderunix.org/?lng=tr&page=services#maillists</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Fedora Kullanıcıları E-posta Listesi: Fedora dağıtımı konusunda bir yardımlaşma listesi olarak faaliyet gösteren fedora-turkey e-posta listesi hakkında detaylı bilgiye http://groups.google.com/group/fedora-turkey adresinden ulaşabilirsiniz. 
+<li type="square">
+<strong>Fedora Kullanıcıları E-posta Listesi: </strong> Fedora dağıtımı konusunda bir yardımlaşma listesi olarak faaliyet gösteren fedora-turkey e-posta listesi hakkında detaylı bilgiye <a href="http://groups.google.com/group/fedora-turkey" target="_blank">http://groups.google.com/group/fedora-turkey</a> adresinden ulaşabilirsiniz. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-GNOME Kullanıcıları Listesi: Popüler masaüstü yöneticisi GNOME konusunda bir yardımlaşma listesi olarak faaliyet gösteren ve GNOME konusunda sorunların paylaşılabildiği e-posta listesi hakkında detaylı bilgiye http://mail.gnome.org/mailman/listinfo/gnome-turk adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>GNOME Kullanıcıları Listesi: </strong> Popüler masaüstü yöneticisi GNOME konusunda bir yardımlaşma listesi olarak faaliyet gösteren ve GNOME konusunda sorunların paylaşılabildiği e-posta listesi hakkında detaylı bilgiye <a href="http://mail.gnome.org/mailman/listinfo/gnome-turk" target="_blank">http://mail.gnome.org/mailman/listinfo/gnome-turk</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-KDE Kullanıcıları Listesi: Popüler masaüstü yöneticisi KDE konusunda, KDE Türkiye bünyesinde bir yardımlaşma listesi olarak faaliyet gösteren ve KDE'ye ilişkin sorunların paylaşılabildiği e-posta listesi hakkında detaylı bilgiye http://www.kde.org.tr/mailman/listinfo/kde adresinden ulaşabilirsiniz. 
+<ul>
+<li type="square">
+<strong>KDE Kullanıcıları Listesi: </strong> Popüler masaüstü yöneticisi KDE konusunda, KDE Türkiye bünyesinde bir yardımlaşma listesi olarak faaliyet gösteren ve KDE'ye ilişkin sorunların paylaşılabildiği e-posta listesi hakkında detaylı bilgiye <a href="http://www.kde.org.tr/mailman/listinfo/kde" target="_blank">http://www.kde.org.tr/mailman/listinfo/kde</a> adresinden ulaşabilirsiniz. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Linux Kullanıcıları Derneği E-posta Listeleri: Linux Kullanıcıları Derneği (LKD) bünyesinde kurulmuş ve 1993 yılından bu yana hizmet veren LKD e-posta listeleri bir çok konuda belirli e-posta listelerine sahiptir. LKD bünyesinde faaliyetine devam etmekte olan listeler hakkında bilgi sahibi olmak, listelere katılmak ve liste arşivlerine dair bilgileri öğrenmek için http://liste.linux.org.tr/listeler.php adresini ziyaret edebilirsiniz. 
+<li type="square">
+<strong>Linux Kullanıcıları Derneği E-posta Listeleri: </strong> Linux Kullanıcıları Derneği (LKD) bünyesinde kurulmuş ve 1993 yılından bu yana hizmet veren LKD e-posta listeleri bir çok konuda belirli e-posta listelerine sahiptir. LKD bünyesinde faaliyetine devam etmekte olan listeler hakkında bilgi sahibi olmak, listelere katılmak ve liste arşivlerine dair bilgileri öğrenmek için <a href="http://liste.linux.org.tr/listeler.php" target="_blank">http://liste.linux.org.tr/listeler.php</a> adresini ziyaret edebilirsiniz. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-OpenOffice.org.tr E-posta Listeleri: Popüler ofis uygulaması OpenOffice.org için OpenOffice.org Türkiye çatısı altında, kullanıcı, duyuru, geliştirme vb. listeler faaliyet göstermektedir. Bu listelere ilişkin detaylı bilgi http://www.openoffice.org.tr/content/view/41/50/ adresinde bulunmaktadır.
+<li type="square">
+<strong>OpenOffice.org.tr E-posta Listeleri: </strong> Popüler ofis uygulaması OpenOffice.org için OpenOffice.org Türkiye çatısı altında, kullanıcı, duyuru, geliştirme vb. listeler faaliyet göstermektedir. Bu listelere ilişkin detaylı bilgi <a href="http://www.openoffice.org.tr/content/view/41/50/" target="_blank">http://www.openoffice.org.tr/content/view/41/50/</a> adresinde bulunmaktadır.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Pardus E-posta Listeleri: TÜBİTAK UEKAE bünyesinde geliştirilen Pardus dağıtımı için, proje bünyesinde bir çok e-posta listesi bulunmaktadır. Listelerin tamamına http://liste.pardus.org.tr adresinden ulaşabileceğiniz gibi, yardımlaşma listesi olan "pardus-kullanıcıları" listesine http://liste.pardus.org.tr/mailman/listinfo/pardus-kullanicilari adresinden ulaşabilirsiniz. 
+<li type="square">
+<strong>Pardus E-posta Listeleri: </strong> TÜBİTAK UEKAE bünyesinde geliştirilen Pardus dağıtımı için, proje bünyesinde bir çok e-posta listesi bulunmaktadır. Listelerin tamamına <a href="http://liste.pardus.org.tr" target="_blank">http://liste.pardus.org.tr</a> adresinden ulaşabileceğiniz gibi, yardımlaşma listesi olan "pardus-kullanıcıları" listesine <a href="http://liste.pardus.org.tr/mailman/listinfo/pardus-kullanicilari" target="_blank">http://liste.pardus.org.tr/mailman/listinfo/pardus-kullanicilari</a> adresinden ulaşabilirsiniz. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-PostgreSQL E-posta Listesi: Özgür yazılım ürünü olan, PostgreSQL veritabanı yönetim sistemine ilişkin yardımlaşma amacı güden "pgsql-tr-genel" listesine http://archives.postgresql.org/pgsql-tr-genel/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>PostgreSQL E-posta Listesi: </strong> Özgür yazılım ürünü olan, PostgreSQL veritabanı yönetim sistemine ilişkin yardımlaşma amacı güden "pgsql-tr-genel" listesine <a href="http://archives.postgresql.org/pgsql-tr-genel/" target="_blank">http://archives.postgresql.org/pgsql-tr-genel/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Tekir E-posta Listeleri: Bir özgür yazılım ürünü olan, Tekir Ticari Otomasyon uygulamasına ilişkin yardımlaşmaya yönelik kurulmuş olan e-posta listelerine http://tekir.com.tr/mailman/listinfo adresinden ulaşabilirsiniz.
- 
-Ubuntu Kullanıcıları E-posta Listesi: Canonical Ltd. desteğiyle geliştirilen Ubuntu dağıtımına ilişkin kurulmuş olan ve yardımlaşma amacı güden "ubuntu-tr" e-posta listesi hakkında detaylı bilgiye https://lists.ubuntu.com/mailman/listinfo/ubuntu-tr adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Tekir E-posta Listeleri: </strong> Bir özgür yazılım ürünü olan, Tekir Ticari Otomasyon uygulamasına ilişkin yardımlaşmaya yönelik kurulmuş olan e-posta listelerine <a href="http://tekir.com.tr/mailman/listinfo" target="_blank">http://tekir.com.tr/mailman/listinfo</a> adresinden ulaşabilirsiniz.
+ <br/>
+<hr noshade="" size="1"/>
+</li>
 
-Vikipedi Kullanıcıları E-posta Listesi: Özgür ansiklopedi Vikipedi'ye ilişkin yardımlaşma amacı güden e-posta listesi hakkında detaylı bilgiye https://lists.wikimedia.org/mailman/listinfo/wikitr-l adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Ubuntu Kullanıcıları E-posta Listesi: </strong> Canonical Ltd. desteğiyle geliştirilen Ubuntu dağıtımına ilişkin kurulmuş olan ve yardımlaşma amacı güden "ubuntu-tr" e-posta listesi hakkında detaylı bilgiye <a href="https://lists.ubuntu.com/mailman/listinfo/ubuntu-tr" target="_blank">https://lists.ubuntu.com/mailman/listinfo/ubuntu-tr</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
+<li type="square">
+<strong>Vikipedi Kullanıcıları E-posta Listesi: </strong> Özgür ansiklopedi Vikipedi'ye ilişkin yardımlaşma amacı güden e-posta listesi hakkında detaylı bilgiye <a href="https://lists.wikimedia.org/mailman/listinfo/wikitr-l" target="_blank">https://lists.wikimedia.org/mailman/listinfo/wikitr-l</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
+</ul>
 
 <h2>Yardımlaşma Forumları</h2>
 
 Buradaki listede yer alan yardımlaşma forumları belirli amaca hizmet eden forumlar arasından seçilmiştir. Aşağı yukarı her forumda GNU/Linux ve diğer özgür yazılımlara ilişkin bölümler olduğu gerçeğini gözardı etmeksizin, her forumdan bahsetmek bu sayfayı amacı dışına çıkaracağı için, belirli forumların tanıtılması daha uygun olacaktır.
 
-Debian Türkiye Forumu: Debian dağıtımına ilişkin bir yardımlaşma platformu görevi gören Debian Türkiye forumları http://www.debian-turkiye.org/ adresinde faaliyet göstermektedir. 
+<ul>
+<li type="square">
+<strong>Debian Türkiye Forumu: </strong> Debian dağıtımına ilişkin bir yardımlaşma platformu görevi gören Debian Türkiye forumları <a href="http://www.debian-turkiye.org/" target="_blank">http://www.debian-turkiye.org/</a> adresinde faaliyet göstermektedir. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Fedora Türkiye Forumu: Fedora dağıtımına ilişkin bir yardımlaşma platformu görevi gören Fedora TR forumları http://www.fedoratr.com/forum/ adresinde faaliyet göstermektedir.
+<li type="square">
+<strong>Fedora Türkiye Forumu: </strong> Fedora dağıtımına ilişkin bir yardımlaşma platformu görevi gören Fedora TR forumları <a href="http://www.fedoratr.com/forum/" target="_blank">http://www.fedoratr.com/forum/</a> adresinde faaliyet göstermektedir.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Linuxnet Forumu: Çeşitli dağıtımlar hakkında bir yardımlaşma platformu görevi gören Linuxnet forumlarına http://www.linuxnet.com.tr/modules.php?name=Forums adresinden ulaşabilirsiniz. 
+<li type="square">
+<strong>Linuxnet Forumu: </strong> Çeşitli dağıtımlar hakkında bir yardımlaşma platformu görevi gören Linuxnet forumlarına <a href="http://www.linuxnet.com.tr/modules.php?name=Forums" target="_blank">http://www.linuxnet.com.tr/modules.php?name=Forums</a> adresinden ulaşabilirsiniz. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Linux Sevenler Forumu: Çeşitli dağıtımlar hakkında bir yardımlaşma platformu görevi gören Linux Sevenler forumlarına http://forum.linux-sevenler.org adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Linux Sevenler Forumu: </strong> Çeşitli dağıtımlar hakkında bir yardımlaşma platformu görevi gören Linux Sevenler forumlarına <a href="http://forum.linux-sevenler.org" target="_blank">http://forum.linux-sevenler.org</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Mandriva Türkiye Forumu: Mandriva dağıtımına ilişkin bir yardımlaşma platformu görevi gören Mandriva Türkiye forumlarına http://forum.mandrivaturkiye.com/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Mandriva Türkiye Forumu: </strong> Mandriva dağıtımına ilişkin bir yardımlaşma platformu görevi gören Mandriva Türkiye forumlarına <a href="http://forum.mandrivaturkiye.com/" target="_blank">http://forum.mandrivaturkiye.com/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Özgürlükİçin Forumu: TÜBİTAK UEKAE bünyesinde geliştirilen Pardus dağıtımı hakkında kapsamlı bir foruma sahip Özgürlükİçin sitesinin forumlarına http://www.ozgurlukicin.com/forum/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Özgürlükİçin Forumu: </strong> TÜBİTAK UEKAE bünyesinde geliştirilen Pardus dağıtımı hakkında kapsamlı bir foruma sahip Özgürlükİçin sitesinin forumlarına <a href="http://www.ozgurlukicin.com/forum/" target="_blank">http://www.ozgurlukicin.com/forum/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Pardus-Linux.Org Forumu: Pardus Kullanıcıları Derneği'nin bir hizmeti olan ve Pardus dağıtımına ilişkin bir forum görevi üstlenen Pardus-Linux.org forumlarına http://forum.pardus-linux.org/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Pardus-Linux.Org Forumu: </strong> Pardus Kullanıcıları Derneği'nin bir hizmeti olan ve Pardus dağıtımına ilişkin bir forum görevi üstlenen Pardus-Linux.org forumlarına <a href="http://forum.pardus-linux.org/" target="_blank">http://forum.pardus-linux.org/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Slackware Türkiye Forumu: Slackware dağıtımına ilişkin bir yardımlaşma platformu görevi gören Slackware Türkiye forumları http://www.slackware.org.tr/index.php?option=com_joomlaboard&Itemid=19 adresinde faaliyet göstermektedir. 
+<li type="square">
+<strong>Slackware Türkiye Forumu: </strong> Slackware dağıtımına ilişkin bir yardımlaşma platformu görevi gören Slackware Türkiye forumları <a href="http://www.slackware.org.tr/index.php?option=com_joomlaboard&Itemid=19" target="_blank">http://www.slackware.org.tr/index.php?option=com_joomlaboard&Itemid=19</a> adresinde faaliyet göstermektedir. 
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Suse Türkiye Forumu: SuSE ve OpenSuSE dağıtımlarına ilişkin bir yardımlaşma platformu görevi gören SuSE Türkiye forumlarına http://www.suse-tr.com/forum/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Suse Türkiye Forumu: </strong> SuSE ve OpenSuSE dağıtımlarına ilişkin bir yardımlaşma platformu görevi gören SuSE Türkiye forumlarına <a href="http://www.suse-tr.com/forum/" target="_blank">http://www.suse-tr.com/forum/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Tekir Forumu: Ticari otomasyon programı Tekir'in yardımlaşma forumu http://www.tekir.com.tr/forum/ adresinde faaliyet göstermektedir.
+<li type="square">
+<strong>Tekir Forumu: </strong> Ticari otomasyon programı Tekir'in yardımlaşma forumu <a href="http://www.tekir.com.tr/forum/" target="_blank">http://www.tekir.com.tr/forum/</a> adresinde faaliyet göstermektedir.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Truva Linux Forumu: Truva Linux dağıtımına ilişkin bir yardımlaşma platformu görevi gören Truva Linux forumlarına http://www.truvalinux.org.tr/forum/ adresinden ulaşabilirsiniz.
+<li type="square">
+<strong>Truva Linux Forumu: </strong> Truva Linux dağıtımına ilişkin bir yardımlaşma platformu görevi gören Truva Linux forumlarına <a href="http://www.truvalinux.org.tr/forum/" target="_blank">http://www.truvalinux.org.tr/forum/</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
 
-Ubuntu Türkiye Forumu: Canonical Ltd. desteğiyle geliştirilen Ubuntu dağıtımına ilişkin kapsamlı bir foruma sahip Ubuntu Türkiye forumlarına http://forum.ubuntu-tr.org adresinden ulaşabilirsiniz.
-
+<li type="square">
+<strong>Ubuntu Türkiye Forumu: </strong> Canonical Ltd. desteğiyle geliştirilen Ubuntu dağıtımına ilişkin kapsamlı bir foruma sahip Ubuntu Türkiye forumlarına <a href="http://forum.ubuntu-tr.org" target="_blank">http://forum.ubuntu-tr.org</a> adresinden ulaşabilirsiniz.
+<br/>
+<hr noshade="" size="1"/>
+</li>
+</ul>
 
 <h2>IRC KANALLARI</h2>
 
-Anlık yardımlaşma bakımından IRC'nin önemi şüphesiz tartışılmazdır. Özgür yazılım projeleri ve destek kanalları genellikle FreeNode ve OFTC sunucuları üzerinde yer almaktadır. Aşağıdaki liste, irc.freenode.org üzerinde yer alan Türkçe yardımlaşma kanallarına ilişkindir. Freenode sunucusuna herhangi bir IRC istemcisi kullanarak veya web üzerinden http://webchat.freenode.net/ adresinden girebilirsiniz.
+Anlık yardımlaşma bakımından IRC'nin önemi şüphesiz tartışılmazdır. Özgür yazılım projeleri ve destek kanalları genellikle FreeNode ve OFTC sunucuları üzerinde yer almaktadır. Aşağıdaki liste, irc.freenode.org üzerinde yer alan Türkçe yardımlaşma kanallarına ilişkindir. Freenode sunucusuna herhangi bir IRC istemcisi kullanarak veya web üzerinden <a href="http://webchat.freenode.net/" target="_blank">http://webchat.freenode.net/</a> adresinden girebilirsiniz.
+<br/>
 
 Arch Linux: #archlinux.tr
 *BSD: #bsd-tr
@@ -83,7 +188,8 @@ Ubuntu: #ubuntu-tr
 <h2>HABER GRUPLARI</h2>
 
 news.newsturkiye.com bünyesindeki os.linux grubu da Türkçe yardım alınabilecek kaynaklar arasında yer almaktadır.
+<p> </p>
 </div>
 <table width="100%">
 </table>
-</td
+</td>

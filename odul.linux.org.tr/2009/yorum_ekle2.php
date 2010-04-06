82b432c5dc9ae638107f044e5110773043ff26df
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-06 21:25:32 +0000

    eski kodun 2009 klonu yerine mfyz'nin hazirladigi kullanilacagina gore bunu kaldiralim

--- odul.linux.org.tr/2009/yorum_ekle2.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "./tpl/header.php";
-if(yorum_ekle($_POST['penguen'],$_POST['yorum_adi'],$_POST['yorum_eposta'],$_POST['yorum']) == "pl"){include "./tpl/yorum_ekle.tmm.tpl.php";}
-elseif(yorum_ekle($_POST['penguen'],$_POST['yorum_adi'],$_POST['yorum_eposta'],$_POST['yorum']) == "pll"){include "./tpl/yorum_ekle.tmmd.tpl.php";}
-else{Header("Location:index.php");}
-include "./tpl/footer.php";
-?>

20babecd68990b43d1582c2b7eb9adcf919fad21
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-10 18:23:07 +0000

    lkdtema-html->lkdtema-php

--- beta.linux.org.tr/lkdtema-html2/index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php'; ?>
-
-<div class="wrapper" id="content">
-  <p>This is content area.</p>
-</div>
-
-<?php include 'footer.php'; ?>
---
+++ beta.linux.org.tr/lkdtema-php/index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+
+<div id="page">
+  <div class="wrapper" id="content">
+    <p>This is content area.</p>
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>

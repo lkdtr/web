a93e168a3b402c612b127ca831b05d56c4354381
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-10 16:43:06 +0000

    html->php

---
+++ beta.linux.org.tr/lkdtema-html2/footer.php
@@ -0,0 +1,4 @@
+
+
+<!-- Finito -->
+</body></html>
---
+++ beta.linux.org.tr/lkdtema-html2/menu.php
@@ -0,0 +1,31 @@
+<div class="wrapper" id="horizontal_menu">
+  <ul id="nav">
+    <li><a href="#">LKD Hakkında</a></li>
+    <li><a href="#">Haberler</a></li>
+    <li>
+      <a href="#">Daha Fazla Linux</a>
+      <ul>
+        <li><a href="#">Linux CD'leri</a></li>
+        <li><a href="#">Destek ve Eğitim Kurumları</a></li>
+        <li><a href="#">Kitaplar</a></li>
+        <li><a href="#">Elektronik Dergiler</a></li>
+        <li><a href="#">Dağıtımlar Kılavuzu</a></li>
+        <li><a href="#">Yerel Özgür Yazılımlar</a></li>
+        <li><a href="#">Yerelleştirme Çalışmaları</a></li>
+        <li><a href="#">Yardım</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Bağlantılar</a>
+      <ul>
+        <li><a href="#">FTP Dosya Alanı</a></li>
+        <li><a href="#">Belgeler</a></li>
+        <li><a href="#">Seminerler</a></li>
+        <li><a href="#">Linux Gezegeni</a></li>
+        <li><a href="#">Linux ve Özgür Yazılım Şenliği</a></li>
+        <li><a href="#">E-posta Listeleri</a></li>
+      </ul>
+    </li>
+    <li><a href="#">İletişim</a></li>
+  </ul>
+</div>

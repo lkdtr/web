6cbb4765849bbe711b68820632e5f6f3b11b8820
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-10 16:43:06 +0000

    html->php

---
+++ beta.linux.org.tr/lkdtema-html2/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Türkiye Linux Kullanıcıları" />
+    <meta name="keywords" content="türkiye, linux" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" />
+  </head>
+  <body>
+    <div id="top">
+      <div class="wrapper" id="header">
+        <a href="http://www.acik-kaynak.org.tr" target="blank"><img class="header_banner" src="images/header_bn.png" /></a>
+      </div>
+      <?php include 'menu.php'; ?>
+    </div>
+
+    <!-- Header part is finished. -->
---
+++ beta.linux.org.tr/lkdtema-html2/index.php
@@ -0,0 +1,7 @@
+<?php include 'header.php'; ?>
+
+<div class="wrapper" id="content">
+  <p>This is content area.</p>
+</div>
+
+<?php include 'footer.php'; ?>

274b5888d7157c1bf8b8c433ee2cb43ce17fef76
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-11 17:25:14 +0000

    son 5 haberi alalim, tagleri attributesleri falan devre disi birakalim.

--- beta.linux.org.tr/lkdtema-php/news.php
+++ beta.linux.org.tr/lkdtema-php/news.php
@@ -5,6 +5,9 @@ include('simplepie.class.php');
 $feed = new SimplePie();
 $feed->set_feed_url('http://haber.linux.org.tr/feed/');
 $feed->init();
+$feed->strip_attributes();
+$feed->strip_comments();
+$feed->strip_htmltags();
 $feed->handle_content_type();
 
 ?>
@@ -12,11 +15,11 @@ $feed->handle_content_type();
 <h4 class="news_title"><a href="#">Haber Linux</a></h4>
 
 <?php if ($feed->data): ?>
-  <?php $items = $feed->get_items(); ?>
-  <span class="news_description">Son <?php echo $feed->get_item_quantity(); ?> haber görüntüleniyor..</span>
+  <?php $items = $feed->get_items(0, 5); ?>
+  <span class="news_description">Son <?php echo count($items); ?> haber görüntüleniyor..</span>
   <ul class="news">
     <?php foreach($items as $item): ?>
-        <li>
+        <li class="new">
         <h4><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a> <?php echo $item->get_date('j M Y'); ?></h4>
         <?php echo $item->get_content(); ?>
         <?php

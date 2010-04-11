405b3cbd85c2aa6fab59488e2158b524ad933a29
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-11 19:05:26 +0000

    Son haberi ayrintili, kalan dokuz haberi ise basliklar halinde listeliyor. Doruk'un onerisi.

--- beta.linux.org.tr/lkdtema-php/news.php
+++ beta.linux.org.tr/lkdtema-php/news.php
@@ -13,20 +13,22 @@ $feed->handle_content_type();
 ?>
 
 <h4 class="news_title"><a href="#">Haber Linux</a></h4>
-
 <?php if ($feed->data): ?>
-  <?php $items = $feed->get_items(0, 5); ?>
-  <span class="news_description">Son <?php echo count($items); ?> haber görüntüleniyor..</span>
+  <?php $items = $feed->get_items(1, 10); ?>
+  <span class="news_description">Son <?php echo count($items) + 1; ?> haber görüntüleniyor..</span>
   <ul class="news">
+    <li class="new">
+      <?php $last_item = $feed->get_item(0); ?>
+      <h4><a href="<?php echo $last_item->get_permalink(); ?>"><?php echo $last_item->get_title(); ?></a> <?php echo $last_item->get_date('j M Y'); ?></h4>
+      <?php echo $last_item->get_content(); ?>
+      <?php
+      if ($enclosure = $last_item->get_enclosure(0))
+        echo '<p><a href="' . $enclosure->get_link() . '" class="download"><img src="./for_the_demo/mini_podcast.png" alt="Podcast" title="Download the Podcast" border="0" /></a></p>';
+      ?>
+    </li>
+    <li><h4 class="other_news">Diğer Haber Başlıkları</h4></li>
     <?php foreach($items as $item): ?>
-        <li class="new">
-        <h4><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a> <?php echo $item->get_date('j M Y'); ?></h4>
-        <?php echo $item->get_content(); ?>
-        <?php
-        if ($enclosure = $item->get_enclosure(0))
-          echo '<p><a href="' . $enclosure->get_link() . '" class="download"><img src="./for_the_demo/mini_podcast.png" alt="Podcast" title="Download the Podcast" border="0" /></a></p>';
-        ?>
-      </li>
+      <li class="other_new"><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a> <?php echo $item->get_date('j M Y'); ?></li>
     <?php endforeach; ?>
   </ul>
   </div>

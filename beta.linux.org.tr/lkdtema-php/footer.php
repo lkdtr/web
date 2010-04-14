f0c3bf23162e02226e88e5b9e7ea21b1a24b85b4
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-14 23:09:49 +0000

    firefox ve bir ornek olarak da VIM dugmelerini ekleyelim.

--- beta.linux.org.tr/lkdtema-php/footer.php
+++ beta.linux.org.tr/lkdtema-php/footer.php
@@ -1,5 +1,8 @@
 <div class="wrapper" id="footer">
-  <p class="left">;-)</p>
+  <p class="left">
+    <a href='http://www.mozilla.com/?from=sfx&amp;uid=0&amp;t=581'><img src='http://sfx-images.mozilla.org/firefox/3.6/80x15_square_orange.png' alt='Spread Firefox Affiliate Button' border='0' /></a>
+    <a href='http://www.vim.org'><img src='images/banners/vim_edited_blue.jpg' alt='Edited with VIM' /></a>
+  </p>
   <span><a href="pages/web-cg.php">Web Çalışma Grubu 2010</a></span>
   <span><a href="pages/gpl.php">GPL</a></span>
   <span><a href="pages/destek.php">Destek</a></span>

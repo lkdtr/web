5d5f4baa5351fe6b45835a84bdf9fda99d17aa8d
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-14 23:46:24 +0000

    simple is the best diyerekten.

--- beta.linux.org.tr/lkdtema-php/index.php
+++ beta.linux.org.tr/lkdtema-php/index.php
@@ -8,18 +8,8 @@
       <a href="#"><img class="button" src="images/button-2.jpg" /></a>
     </div>
     <?php include 'news.php'; ?>
-    <div id="bottom_right">
-      <div id="other">
-        <p>Burası firefox banneri falan için.</p>
-      </div>
-      <div id="supports">
-        <h4>Sunucu Destekçilerimiz</h4>
-        <a href="http://www.parkyeri.com"><img src="images/supports/ulakbim.gif" /></a>
-        <a href="http://www.ulakbim.gov.tr"><img src="images/supports/parkyeri.gif" /></a>
-      </div>
-    </div>
-    <?php include 'google_search.php'; ?>
   </div>
+  <?php include 'bottom_area.php'; ?>
   <?php include 'footer.php'; ?>
 </div>
 

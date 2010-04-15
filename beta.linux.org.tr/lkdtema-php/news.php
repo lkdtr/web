65f9cfb522eec09699d3d7ba0df4a7985fdb2f77
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-15 00:38:48 +0000

    arama bolumu daha minimal oldu; ama sunucu destekcilerimiz kismi icin henuz bir stil uyduramadim.

--- beta.linux.org.tr/lkdtema-php/news.php
+++ beta.linux.org.tr/lkdtema-php/news.php
@@ -12,23 +12,20 @@ $feed->handle_content_type();
 
 ?>
 
-<h4>Haber Linux</h4>
 <?php if ($feed->data): ?>
+  <?php $last_item = $feed->get_item(0); ?>
+  <h4><a href="<?php echo $last_item->get_permalink(); ?>"><?php echo $last_item->get_title(); ?></a> <?php echo $last_item->get_date('j M Y'); ?></h4>
+  <?php echo $last_item->get_content(); ?>
+  <?php
+  if ($enclosure = $last_item->get_enclosure(0))
+    echo '<p><a href="' . $enclosure->get_link() . '" class="download"><img src="./for_the_demo/mini_podcast.png" alt="Podcast" title="Download the Podcast" border="0" /></a></p>';
+    ?>
+
   <?php $items = $feed->get_items(1, 2); ?>
+  <h4>Diğer Haber Başlıkları</h4>
   <ul class="news">
-    <li class="new">
-      <?php $last_item = $feed->get_item(0); ?>
-      <h4 class="new_title"><a href="<?php echo $last_item->get_permalink(); ?>"><?php echo $last_item->get_title(); ?></a> <?php echo $last_item->get_date('j M Y'); ?></h4>
-      <?php echo $last_item->get_content(); ?>
-      <?php
-      if ($enclosure = $last_item->get_enclosure(0))
-        echo '<p><a href="' . $enclosure->get_link() . '" class="download"><img src="./for_the_demo/mini_podcast.png" alt="Podcast" title="Download the Podcast" border="0" /></a></p>';
-      ?>
-    </li>
-    <li><h4 class="other_news">Diğer Haber Başlıkları</h4></li>
     <?php foreach($items as $item): ?>
       <li class="other_new"><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a> <?php echo $item->get_date('j M Y'); ?></li>
     <?php endforeach; ?>
   </ul>
 <?php endif; ?>
-

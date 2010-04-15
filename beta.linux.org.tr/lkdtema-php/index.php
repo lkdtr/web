8b0b2490158ade526f51083b470647ec952252c4
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-15 00:38:48 +0000

    arama bolumu daha minimal oldu; ama sunucu destekcilerimiz kismi icin henuz bir stil uyduramadim.

--- beta.linux.org.tr/lkdtema-php/index.php
+++ beta.linux.org.tr/lkdtema-php/index.php
@@ -8,6 +8,11 @@
       <a href="#"><img class="button" src="images/button-2.jpg" /></a>
     </div>
     <?php include 'news.php'; ?>
+    <h4>Sunucu Destekçilerimiz</h4>
+    <ul class="supports">
+      <li><a href="http://www.ulakbim.gov.tr">Ulusal Akademik Ağ ve Bilgi Merkezi</a></li>
+      <li><a href="http://www.parkyeri.com">Parkyeri</a></li>
+    </ul>
   </div>
   <?php include 'bottom_area.php'; ?>
   <?php include 'footer.php'; ?>

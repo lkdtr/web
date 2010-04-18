a4f068956316479773146361da4a17177b3a2795
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-18 17:51:16 +0000

    footer baglantilarini da duzeltelim.

--- beta.linux.org.tr/wp-content/themes/lkdtema/footer.php
+++ beta.linux.org.tr/wp-content/themes/lkdtema/footer.php
@@ -10,7 +10,6 @@
     <a href='http://www.mozilla.com/?from=sfx&amp;uid=0&amp;t=581'><img src='http://sfx-images.mozilla.org/firefox/3.6/80x15_square_orange.png' alt='Spread Firefox Affiliate Button' border='0' /></a>
     <a href='http://www.vim.org'><img src='<?php bloginfo('template_url'); ?>/images/banners/vim_edited_blue.jpg' alt='Edited with VIM' /></a>
   </p>
-  <span><a href="pages/web-cg.php">Web Çalışma Grubu 2010</a></span>
-  <span><a href="pages/gpl.php">GPL</a></span>
-  <span><a href="pages/destek.php">Destek</a></span>
+  <span><a href="<?php bloginfo('siteurl'); ?>/web-cg">Web Çalışma Grubu 2010</a></span>
+  <span><a href="<?php bloginfo('siteurl'); ?>/gpl">GPL</a></span>
 </div>

32ccffa9d8612c4e75de0122303c4005ea49fff4
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-18 10:35:34 +0000

    header, menu ve banner kismi aktarildi. style.css de tekrar eklendi.

---
+++ beta.linux.org.tr/wp-content/themes/lkdtema/menu.php
@@ -0,0 +1,41 @@
+<div class="wrapper" id="horizontal_menu">
+  <ul id="nav">
+    <li>
+      <a href="#">Daha Fazla Linux</a>
+      <ul>
+        <li><a href="#">Linux CD'leri</a></li>
+        <li><a href="#">Destek ve Eğitim Kurumları</a></li>
+        <li><a href="#">Kitaplar</a></li>
+        <li><a href="#">Elektronik Dergiler</a></li>
+        <li><a href="#">Dağıtımlar Kılavuzu</a></li>
+        <li><a href="#">Yerel Özgür Yazılımlar</a></li>
+        <li><a href="#">Yerelleştirme Çalışmaları</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Daha Fazla Özgürlük</a>
+      <ul>
+        <li><a href="#">Wikipedia Özgür Ansiklopedi</a></li>
+        <li><a href="#">Özgür Dünya Haritası</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Bağlantılar</a>
+      <ul>
+        <li><a href="#">FTP Dosya Alanı</a></li>
+        <li><a href="#">Belgeler</a></li>
+        <li><a href="#">Seminerler</a></li>
+        <li><a href="#">Linux Gezegeni</a></li>
+        <li><a href="#">Linux ve Özgür Yazılım Şenliği</a></li>
+        <li><a href="#">E-posta Listeleri</a></li>
+      </ul>
+    </li>
+    <li class="last"><a class="help" href="#">Yardım</a></li>
+  </ul>
+
+  <ul id="nav_en">
+    <li><a href="#">LKD Hakkında</a></li>
+    <li><a href="#">About Us</a></li>
+    <li class="last"><a href="#">İletişim</a></li>
+  </ul>
+</div>

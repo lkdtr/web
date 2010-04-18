1c738680a72c95b48a4904064ebc8dc7a8f469f8
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-18 13:17:04 +0000

    kafa karisikligi.. yaptigim komutlar nereye gitti bilmiyorum o_O. buttons.js'deki hatayi tekrar duzelt.

--- beta.linux.org.tr/wp-content/themes/lkdtema/javascript/buttons.js
+++ beta.linux.org.tr/wp-content/themes/lkdtema/javascript/buttons.js
@@ -1,7 +1,7 @@
 $(function() {
     $("img.button")
         .mouseover(function() {
-            var src = $(this).attr("src").match(/[^\.]+/) + "_hover.jpg";
+            var src = $(this).attr("src").replace(".jpg", "_hover.jpg");
             $(this).attr("src", src);
         })
         .mouseout(function() {

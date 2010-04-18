994a156448eca68f5edd248100790134f5034751
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-04-18 13:26:04 +0000

    footer'i ekleyelim.

--- beta.linux.org.tr/wp-content/themes/lkdtema/footer.php
+++ beta.linux.org.tr/wp-content/themes/lkdtema/footer.php
@@ -1,33 +1,16 @@
 <?php
 /**
  * @package WordPress
- * @subpackage Default_Theme
+ * @subpackage lkdTema
  */
 ?>
 
-<div id="alt">
-
-<div class="icerik">
-
-<div id="altbilgiler">
-		<?php bloginfo('name'); ?>,
-    <a href="http://wordpress.org/">WordPress</a> - <a href="http://www.wordpress-tr.com/">TR</a> üzerine kurulmuştur.
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Yazılar (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Yorumlar (RSS)</a>.
-		<!-- <?php echo get_num_queries(); ?> sorgu. <?php timer_stop(1); ?> saniye. -->
+<div class="wrapper" id="footer">
+  <p class="left">
+    <a href='http://www.mozilla.com/?from=sfx&amp;uid=0&amp;t=581'><img src='http://sfx-images.mozilla.org/firefox/3.6/80x15_square_orange.png' alt='Spread Firefox Affiliate Button' border='0' /></a>
+    <a href='http://www.vim.org'><img src='<?php bloginfo('template_url'); ?>/images/banners/vim_edited_blue.jpg' alt='Edited with VIM' /></a>
+  </p>
+  <span><a href="pages/web-cg.php">Web Çalışma Grubu 2010</a></span>
+  <span><a href="pages/gpl.php">GPL</a></span>
+  <span><a href="pages/destek.php">Destek</a></span>
 </div>
-
-<div id="altlogo">
-<img src="<?php bloginfo('stylesheet_directory'); ?>/images/tlkdaltlogo.gif" />
-</div>
-
-<?php /* "Sen ne yaptığını sanıyorsun Dave?" */ ?>
-
-		<?php wp_footer(); ?>
-        
-</div>
-
-</div>
-
-</body>
-</html>

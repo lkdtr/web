9a2b2aa6b82472b2f79b44e9e511484887b5e705
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-04-18 17:16:02 +0000

    sayfa ve teksayfalari da temaya uyduralim.

--- beta.linux.org.tr/wp-content/themes/lkdtema/page.php
+++ beta.linux.org.tr/wp-content/themes/lkdtema/page.php
@@ -1,31 +1,17 @@
 <?php
 /**
  * @package WordPress
- * @subpackage Default_Theme
+ * @subpackage lkdTema
  */
 
-get_header(); ?>
-<div id="orta">
-	<div id="content" class="narrowcolumn">
+  get_header();
+?>
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="post" id="post-<?php the_ID(); ?>">
-		<h2><?php the_title(); ?></h2>
-			<div class="entry">
-				<?php the_content('<p class="serif">Bu sayfanın tamamını okuyun &raquo;</p>'); ?>
-
-				<?php wp_link_pages(array('before' => '<p><strong>Sayfalar:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-
-			</div>
-		</div>
-		<?php endwhile; endif; ?>
-	<?php edit_post_link('Sayfayı düzenleyin.', '<p>', '</p>'); ?>
-	</div>
-    </div>
-<?php get_sidebar(); ?>
-
+<div id="page">
+  <div class="wrapper" id="content">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+  </div>
+  <?php get_footer(); ?>
 </div>
-
-<div class="temizle"></div>
-
-<?php get_footer(); ?>

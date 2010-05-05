2a20008d8e7aad98daf3bb7885b6dadc81768ff1
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-05-05 19:45:05 +0000

    buna da favicon ekleyelim.

--- www.lkd.org.tr/wp-content/themes/lkdtema/header.php
+++ www.lkd.org.tr/wp-content/themes/lkdtema/header.php
@@ -11,7 +11,7 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-
+<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/favicon.png" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />

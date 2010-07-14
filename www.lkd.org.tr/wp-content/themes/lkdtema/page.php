d4e5c872a99c38e96355be1cae7b877d2d08135c
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-07-14 16:37:37 +0000

    sevgili baslik, tat kaciriyorsun, sen temadan gelme, biz gerekirse seni sayfalara elle ekleriz

--- www.lkd.org.tr/wp-content/themes/lkdtema/page.php
+++ www.lkd.org.tr/wp-content/themes/lkdtema/page.php
@@ -10,7 +10,6 @@ get_header(); ?>
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
-		<h2><?php the_title(); ?></h2>
 			<div class="entry">
 				<?php the_content('<p class="serif">Bu sayfanın tamamını okuyun &raquo;</p>'); ?>
 

12381943a3d29503936764df70456da4f318e0cd
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-07-19 12:32:01 +0000

    yeni tasarım

--- www.linux.org.tr/wp-content/themes/linuxtema/news.php
+++ www.linux.org.tr/wp-content/themes/linuxtema/news.php
@@ -1,32 +1,58 @@
 <?php
 /*
-Template Name: News
+ * Template Name: News
+ *
+ * exceprt($limit) fonksiyonunun çalışması için şu eklentiye ihtiyaç var:
+ * http://wordpress.org/extend/plugins/content-and-excerpt-word-limit/
  */
 ?>
 
 
 <?php get_header(); ?>
+<?php
+
+    if (isset($_GET['paged'])) $cur_page = absint($_GET['paged']);
+    else $cur_page = 1;
+
+    $offset = ($cur_page - 1) * 10;
+    $previous = $cur_page - 1;
+    $next = $cur_page + 1;
+
+    query_posts(array('showposts' => '10', 'offset' => $offset));
+
+?>
 
 <div id="page">
   <div class="wrapper">
     <div id="content-news">
-        <h2>Haberler</h2>
-        <ul>
-          <?php
-            if (isset($_GET['paged'])) $cur_page = absint($_GET['paged']);
-            else $cur_page = 1;
-
-            $offset = ($cur_page - 1) * 10;
-            query_posts(array('showposts' => '10', 'offset' => $offset)); ?>
+      <h2>Haberler</h2>
+        <ul class="content-news-top">
+          <?php delete_option('wl_readmore_link'); ?>
           <?php while (have_posts()) : the_post(); ?>
-          <li>
-            <p class="time">(<?php the_time('d F Y'); ?>)</p>
-            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+          <li class="top">
+              <ul>
+                  <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+                  <li class="bottom">
+                      <h4><?php the_title(); ?></h4>
+                      <p class="time">(<?php the_time('d F Y'); ?>)</p>
+                      <div class="entry">
+                          <p><?php excerpt(25); ?></p>
+                      </div>
+                  </li>
+                  </a>
+                  <li class="comments">
+                      <?php comments_popup_link(__('No Comments'), __('1 Comment'), __('% Comments')); ?>
+                  </li>
+              </ul>
           </li>
           <?php endwhile; ?>
         </ul>
-        <div class="navigation">
-            <?php posts_nav_link(); ?>
+        <div id="navigation">
+            <?php if ($previous != 0) : ?>
+                <p class="previous"><a href="/haberler/?paged=<?php echo $previous; ?>"><- Önceki Sayfa</a></p>
+            <?php endif; ?>
+            <p class="next"><a href="/haberler/?paged=<?php echo $next; ?>">Sonraki Sayfa -></a></p>
+            <div style="clear: both">
         </div>
     </div> <!-- end content -->
   </div> <!-- end wrapper -->

2b69ddd1da691be7df0ba39e1a240e6762d5b966
Umuthan Uyan <mail@umuthan.com>
2010-07-30 20:56:06 +0000

    paylas, organizasyon, sponsorlar bolumu

---
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/sidebar.php
@@ -0,0 +1,15 @@
+<h2>ORGANIZASYON</h2>
+<a href="http://www.lkd.org.tr" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/tlkdorganizasyonlogo.png" /></a><br />
+<a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/ytucorganizasyonlogo.png" /></a>
+<div class="temizle"></div>
+
+<h2>PAYLAS</h2>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/facebooklogo.png" /></a>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/xinglogo.png" /></a>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/deliciouslogo.png" /></a>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/twitterlogo.png" /></a><br />
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/linkedinlogo.png" /></a>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/digglogo.png" /></a>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/googlelogo.png" /></a>
+<a href="#"><img src="<?php bloginfo('template_url'); ?>/resimler/friendfeedlogo.png" /></a>
+<div class="temizle"></div>

b8e5f8b650edf5ce792bb59d73f0fdc4555b8568
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-07-30 10:37:52 +0000

    temanin beta versiyonunu :) yukleyelim

---
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/footer.php
@@ -0,0 +1,6 @@
+<div id="alt">
+<a href="http://www.lkd.org.tr" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/tlkdlogo.png" /></a>
+</div>
+
+</body>
+</html>
---
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Ana Tema dosyasi
+ *
+ * @package WordPress
+ * @subpackage owtgtema
+ */ ?>
+
+<?php get_header(); ?>
+
+<div id="solbolum"><?php get_sidebar(); ?></div>
+
+<div id="ortabolum">
+<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+
+			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				<small><?php the_time('d F Y') ?> <!-- <?php the_author() ?> tarafından --></small>
+
+				<div class="entry">
+					<?php the_content('Yazının kalanını okuyun &raquo;'); ?>
+				</div>
+
+				<p class="postmetadata"><?php the_tags('Etiketler: ', ', ', '<br />'); ?> Kategori <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('Yorum Yok &#187;', '1 Yorum &#187;', '% Yorum &#187;'); ?></p>
+			</div>
+
+		<?php endwhile; ?>
+
+	<?php else : ?>
+
+		<h2 class="center">Bulunamadı</h2>
+		<p class="center">Üzgünüz, aradığınız şey burada değil.</p>
+		<?php get_search_form(); ?>
+
+<?php endif; ?>
+</div>
+
+<div id="sagbolum"><?php wp_list_bookmarks(); ?></div>
+
+<?php get_footer(); ?>

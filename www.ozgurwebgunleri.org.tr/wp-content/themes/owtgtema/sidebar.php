7c3a983e5719dfc46bbaf61d5def144b59f8bcff
Umuthan Uyan <mail@umuthan.com>
2010-07-31 11:15:36 +0000

    yazilardan yazar ve tarih silindi etkinlik tarihi sol tarafa eklendi orta bolum hizzalandi

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/sidebar.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/sidebar.php
@@ -21,6 +21,7 @@
 </div>
 
 <div id="gnu"></div>
+<div align="center"><img src="<?php bloginfo('template_url'); ?>/resimler/etkinliktarihi.jpg" /></div>
 <div id="ulasim">
 <div class="icerik">
 yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe universitesi yeditepe universitesi ulasim tarifi yeditepe universitesi ulasim tarifi yeditepe univer

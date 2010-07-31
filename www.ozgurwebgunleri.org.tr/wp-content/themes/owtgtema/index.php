03ed8390cd55e4d4d5beecf527d10a2b20a2e111
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-07-31 11:15:36 +0000

    yazilardan yazar ve tarih silindi etkinlik tarihi sol tarafa eklendi orta bolum hizzalandi

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/index.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/index.php
@@ -19,7 +19,6 @@
 
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<small><?php the_time('d F Y') ?> <!-- <?php the_author() ?> tarafından --></small>
 
 				<div class="entry">
 					<?php the_content('Yazının kalanını okuyun &raquo;'); ?>

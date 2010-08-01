48692ada12649283554d6e2d8bba24fc418b477e
Umuthan Uyan <mail@umuthan.com>
2010-08-01 21:14:35 +0000

    footerdaki twitter

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/footer.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/footer.php
@@ -4,6 +4,7 @@
 <a href="http://www.facebook.com/event.php?eid=114749385242161" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/facebooklogo.png" width="16px" /></a>
 <a href="http://tuxweet.linux.org.tr/ozgurwebgunleri" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/tuxweetlogo.png" width="16px" /></a>
 <a href=" https://www.xing.com/events/ozgur-web-teknolojileri-gunleri-543102" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/xinglogo.png" width="16px" /></a>
+<a href="http://twitter.com/ozgurwebgunleri" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/twitterlogo.png" width="16px" /></a>
 </div>
 <div class="temizle"></div>
 </div>

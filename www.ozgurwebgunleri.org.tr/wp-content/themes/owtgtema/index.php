6f372956ed5c6e1237c91ca7184ac4e318457e02
Umuthan Uyan <mail@umuthan.com>
2010-08-01 22:35:59 +0000

    anasayfada iken bilgiler basligi gorunmesin

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/index.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/index.php
@@ -18,7 +18,7 @@
 		<?php while (have_posts()) : the_post(); ?>
 
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-				<? if($_SERVER['REQUEST_URI']=="/") { ?><h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2><? } ?>
+				<? if($_SERVER['REQUEST_URI']!="/") { ?><h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2><? } ?>
 
 				<div class="entry">
 					<?php the_content('Yazının kalanını okuyun &raquo;'); ?>

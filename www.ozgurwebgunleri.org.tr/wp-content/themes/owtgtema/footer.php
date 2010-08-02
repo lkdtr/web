5ce64e77f6877d4667167be1fb5e50a17060f533
Umuthan Uyan <mail@umuthan.com>
2010-08-02 08:14:38 +0000

    takip et bolumune linkedin eklendi orta taraf icin li elementi resmi konuldu.

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/footer.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/footer.php
@@ -1,6 +1,7 @@
 <div id="alt">
 <div id="tlkdlogo"><a href="http://www.lkd.org.tr" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/tlkdlogo.png" /></a></div>
 <div id="sosyalaglar">
+<a href="http://events.linkedin.com/Ozgur-Web-Teknolojileri-Gunleri/pub/383023" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/linkedinlogo.png" width="16px" /></a>
 <a href="http://www.facebook.com/event.php?eid=114749385242161" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/facebooklogo.png" width="16px" /></a>
 <a href="http://tuxweet.linux.org.tr/ozgurwebgunleri" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/tuxweetlogo.png" width="16px" /></a>
 <a href=" https://www.xing.com/events/ozgur-web-teknolojileri-gunleri-543102" target="_blank"><img src="<?php bloginfo('template_url'); ?>/resimler/xinglogo.png" width="16px" /></a>

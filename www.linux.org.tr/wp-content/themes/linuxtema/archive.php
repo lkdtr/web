6ca661024314242bc43bb7c6b4b22f7961de52ee
gokmen.gorgen <gokmen.gorgen@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-08-02 18:59:56 +0000

    tarih stilini değiştirek

--- www.linux.org.tr/wp-content/themes/linuxtema/archive.php
+++ www.linux.org.tr/wp-content/themes/linuxtema/archive.php
@@ -24,8 +24,8 @@
      	  
      	  <?php while (have_posts()) : the_post(); ?>
       		<div <?php post_class() ?>>
-      				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-      				<small><?php the_time('d F Y'); ?></small>
+      				<h3 id="post-<?php the_ID(); ?>" style="margin-bottom: 0"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+              <p class="time">(<?php the_time('d F Y'); ?>)</p>
 
       				<div class="entry">
       					<?php the_content() ?>

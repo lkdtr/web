a28ebe18f7f8258cd0a4837b84f2e8d96e1c4837
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-08-02 18:48:12 +0000

    aya göre arama olayını da ekleyelim.

--- www.linux.org.tr/wp-content/themes/linuxtema/news_menu.php
+++ www.linux.org.tr/wp-content/themes/linuxtema/news_menu.php
@@ -1,4 +1,11 @@
 <div id="content-news-header">
+  <select name="archive-dropdown" style="float: right; margin: 5px 10px;"
+    onChange='document.location.href=this.options[this.selectedIndex].value;'>
+    <option value="">
+      <?php echo attribute_escape('Aya göre haberleri listele'); ?>
+    </option>
+    <?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?>
+  </select>
   <h2 class="content-news">Haberler</h2>
   <p>
     <a href="/haberler/yeni-haber-girisi">Yeni Haber Gir</a>

e43ac71ad3468dc5f2dfff9d04cf446db8dcb4f8
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-08-05 13:23:18 +0000

    afis ve kimler bolumu revizyon :)

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/sidebar.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/sidebar.php
@@ -25,4 +25,4 @@ $paylasilacaktitle="Özgür Web Teknoloji Günleri 2010"; ?>
 
 <div id="gnu"></div>
 <div class="afisbanner"><a href="#">AFIS&nbsp;&&nbsp;BANNER</a></div>
-<div align="right"><img src="<?php bloginfo('template_url'); ?>/resimler/afiskucuk.jpg" /></div>
+<div align="center"><img src="<?php bloginfo('template_url'); ?>/resimler/afiskucuk.jpg" /></div>

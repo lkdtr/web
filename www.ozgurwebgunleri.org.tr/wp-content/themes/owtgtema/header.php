5804a8cb22d9d89aa5d0a8a0c0c0adb44b933ae4
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-08-16 11:21:04 +0000

    rss i tuxweet e dondurelim

--- www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/header.php
+++ www.ozgurwebgunleri.org.tr/wp-content/themes/owtgtema/header.php
@@ -14,8 +14,7 @@
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/resimler/favicon.png" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
-<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="http://tuxweet.linux.org.tr/rss/username:ozgurwebgunleri" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 <link rel="image_src" href="<?php bloginfo('template_url'); ?>/resimler/logo.jpg" />

a4affbe7ae4cb7370dd76b8b3817a32022f72e6d
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-11-29 11:57:01 +0000

    ozgur yazilim ve linux gunleri 2011 giris temasi

---
+++ www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg-giris/index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div id="logo">
+	<p>Özgür Yazılım ve Linux Günleri 2011
+		1-2 Nisan 2011
+		İstanbul Bilgi Üniversitesi Dolapdere Kampüsü</p>
+</div>
+
+<?php get_footer(); ?>

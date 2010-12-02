6a4f118c41da7d5a65afd598dc184dde31e93d5c
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-12-02 21:03:25 +0000

    bosluklari duzenleyelim, sitesearch'suz de olmuyor

--- liste.linux.org.tr/arsiv.php
+++ liste.linux.org.tr/arsiv.php
@@ -6,8 +6,8 @@
   
   <div>
     <ul>
-     <li><a href="arsivler_1993-2001/">Aralık 1993 - Agustos 2001 Linux Listesi</a></li>
-     <li><a href="/arsivler_2002-2004/">Şubat 2002 - Ekim 2004 - Tüm Linux Listeleri</a></li>
+     <li><a href="arsivler_1993-2001/">Aralık 1993 - Agustos 2001 Linux Listesi</a><br>&nbsp;</li>
+     <li><a href="/arsivler_2002-2004/">Şubat 2002 - Ekim 2004 - Tüm Linux Listeleri</a><br>&nbsp;</li>
      <li><a href="/pipermail/">Ekim 2004 - Bugün</a><br /></li>
   </div>
   
@@ -16,6 +16,7 @@
     <form method="get" action="http://www.google.com/custom">
       <input type="hidden" name="cof" value="S:http://www.linux.org.tr;AH:center;LH:120;L:http://www.lkd.org.tr/images/r1c1.gif;LW:279;AWFID:addc628b01ef0105;">
       <input type="hidden" name="domains" value="liste.linux.org.tr">
+      <input type="hidden" name="sitesearch" value="liste.linux.org.tr">
       <a href="http://www.google.com/search"><img src="http://www.google.com/logos/Logo_40wht.gif" border="0" alt="google" align="middle"></a>
       <input type="text" name="q" size="31" maxlength="255" value="">
       <input type="submit" name="sa" value="Google İle Ara">

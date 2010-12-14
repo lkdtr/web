aed8a6e88150fc1fed7d3c50194d8f8db592c674
Umuthan Uyan <mail@umuthan.com>
2010-12-14 15:15:33 +0000

    Sosyal aglar

--- www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg-giris/index.php
+++ www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg-giris/index.php
@@ -1,9 +1,17 @@
 <?php get_header(); ?>
 
 <div id="logo">
+	<div id="sosyalaglar">
+		<a href="http://tuxweet.linux.org.tr/ozguryazilimgunleri"><img src="<?php bloginfo( 'template_url' ); ?>/images/tuxweet.png"></a>
+		<a href="http://identi.ca/ozguryazilimgunleri"><img src="<?php bloginfo( 'template_url' ); ?>/images/identica.png"></a>
+		<a href="http://friendfeed.com/ozguryazilimgunleri"><img src="<?php bloginfo( 'template_url' ); ?>/images/friendfeed.png"></a>
+		<a href="http://twitter.com/Ozgryzlmgunleri"><img src="<?php bloginfo( 'template_url' ); ?>/images/twitter.png"></a>
+		<a href="http://www.facebook.com/event.php?eid=135560503168688"><img src="<?php bloginfo( 'template_url' ); ?>/images/facebook.png"></a>
+	</div>
 	<p>Özgür Yazılım ve Linux Günleri 2011
 		1-2 Nisan 2011
 		İstanbul Bilgi Üniversitesi Dolapdere Kampüsü</p>
+	<div id="sponsordosyasi">
 </div>
 
 <?php get_footer(); ?>

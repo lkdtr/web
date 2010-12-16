aac5665ed0392935a1fe77d04157d226f58ad755
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2010-12-16 16:54:45 +0000

    OYLG 2011 tema style.css index.php fix

--- www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/index.php
+++ www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/index.php
@@ -12,6 +12,10 @@
 				<?php while (have_posts()) : the_post(); ?>
 
 					<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+						
+						<?php if ( is_home() || is_front_page() ) { ?>
+							<h1><?php the_title(); ?></h1>
+						<?php } ?>
 
 						<div class="entry">
 							<?php the_content('Yazının kalanını okuyun &raquo;'); ?>

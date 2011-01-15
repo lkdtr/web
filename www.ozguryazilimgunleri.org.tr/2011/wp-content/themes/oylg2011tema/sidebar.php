8f95415c6f04b7a7f52feba877ed6dbf0c40fd8e
Umuthan Uyan <mail@umuthan.com>
2011-01-15 23:00:47 +0000

    follow us is not necessary for english page

--- www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/sidebar.php
@@ -1,4 +1,5 @@
 <?php dynamic_sidebar( 'secondary-widget-area' ); ?>
+<? if($_GET["lang"]!="en") { ?>
 <h2><? if($_GET["lang"]=="en") echo "FOLLOW US"; else echo "TAKİP ET"; ?></h2>
 <?php wp_list_bookmarks("title_before=<h2 style='display:none;'>&title_li=&category_before=&category_after=&category=4&orderby=rating"); ?>
 <div class="clearfix"></div>
@@ -12,5 +13,6 @@
 	</div>
 	<div id="bottom"></div>
 </div>
+<? } ?>
 <div align="center"><a href="http://www.ozguryazilimgunleri.org/2011/gorseller"><img src="http://www.ozguryazilimgunleri.org/2011/wp-content/uploads/afis<? if($_GET["lang"]=="en") echo "_en"; ?>.jpg" alt="Özgür Yazılım Günleri 2011 Gorseller" /></a></div>
 <div align="center"><a href="http://www.ozguryazilimgunleri.org/2011/wp-content/uploads/<? if($_GET["lang"]=="en") echo "ozgur_yazilim_ve_linux_gunleri_2011_sponsorluk_dosyasi_web_en.pdf"; else echo "2010/12/ozgur_yazilim_ve_linux_gunleri_2011_sponsorluk_dosyasi_web_tr.pdf"; ?>" target="_blank"><img src="http://www.ozguryazilimgunleri.org/2011/wp-content/uploads/sponsordosyasi<? if($_GET["lang"]=="en") echo "_en"; ?>.jpg" /></a></div>

c7da03db9269d26bf70f21e2169560e8aafbee7d
Umuthan Uyan <mail@umuthan.com>
2011-01-15 12:45:00 +0000

    category name fix

--- www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/sidebar.php
@@ -1,4 +1,4 @@
 <?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-<?php wp_list_bookmarks("category_name='SPONSORLAR'&title_li=&category_before=&category_after=&category=4&orderby=rating"); ?>
+<?php wp_list_bookmarks("category_name='SPONSORLAAR'&title_li=&category_before=&category_after=&category=4&orderby=rating"); ?>
 <div class="clearfix"></div>
 <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>

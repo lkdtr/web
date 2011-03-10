4d75316ecfa3e2fe9bcaa143f0730a10adec2d9c
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2011-03-10 00:20:20 +0000

    komit

--- www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2011/wp-content/themes/oylg2011tema/sidebar.php
@@ -1,7 +1,6 @@
 <h2><? if($_GET["lang"]=="en") echo "MAIN SPONSORS"; else echo "ANA SPONSORLAR"; ?></h2>
 <?php wp_list_bookmarks("title_before=<h2 style='display:none;'>&title_li=&category_before=&category_after=&category=8&orderby=rating"); ?>
 <div class="clearfix"></div>
-<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
 <? if($_GET["lang"]!="en") { ?>
 <div id="tuxweet">
 	<div id="top"></div>
@@ -14,4 +13,4 @@
 	<div id="bottom"></div>
 </div>
 <? } ?>
-<div align="center"><a href="http://www.ozguryazilimgunleri.org/2011/wp-content/uploads/<? if($_GET["lang"]=="en") echo "ozgur_yazilim_ve_linux_gunleri_2011_sponsorluk_dosyasi_web_en.pdf"; else echo "2010/12/ozgur_yazilim_ve_linux_gunleri_2011_sponsorluk_dosyasi_web_tr.pdf"; ?>" target="_blank"><img src="http://www.ozguryazilimgunleri.org/2011/wp-content/uploads/sponsordosyasi<? if($_GET["lang"]=="en") echo "_en"; ?>.jpg" style="margin:10px 0 0 0;" /></a></div>
+<?php dynamic_sidebar( 'secondary-widget-area' ); ?>

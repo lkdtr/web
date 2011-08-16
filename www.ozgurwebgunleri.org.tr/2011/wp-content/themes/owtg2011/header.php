b98e908fc619288f747618cb4e5a5db41859d21c
Umuthan Uyan <mail@umuthan.com>
2011-08-16 14:42:26 +0000

    owtg 2011 tema v0.1

---
+++ www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+
+	?></title>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
+</head>
+
+<body>
+<div id="header">
+	<div id="left">
+		<ul id="organization">
+			<li><a href="http://www.lkd.org.tr/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/tlkd.png" /></a></li>
+			<li><a href="http://www.yucomp.org/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/7tepe.png" /></a></li>
+		</ul>
+		<ul id="followus">
+			<li class="linkedin"><a href=""><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+		</ul>
+		<ul id="leftmenu" class="menu">
+			<li class="home current"><a href="#">ANASAYFA</a></li>
+		</ul>
+	</div>
+	<div id="logo"><img src="<?php bloginfo('template_url'); ?>/images/logo.png" /></div>
+	<div id="right">
+		<div id="tuxweet">
+			
+		</div>
+		<ul id="shareus">
+			<li class="linkedin"><a href=""><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+		</ul>
+		<ul id="rightmenu" class="menu">
+			<li class="home current"><a href="#">ANASAYFA</a></li>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div id="main">

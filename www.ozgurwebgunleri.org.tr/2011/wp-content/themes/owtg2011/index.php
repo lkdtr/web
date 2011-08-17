df39a27686800fad23b346a08901b747ff6a4f98
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2011-08-17 21:10:36 +0000

    index fix 2

--- www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/index.php
+++ www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/index.php
@@ -4,7 +4,7 @@
 <?php while (have_posts()) : the_post(); ?>
 
 <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	<? if(php bloginfo('url')!=$_SERVER['REQUEST_URI']) { ?><h1><?php the_title(); ?></h1><? } ?>
+	<? if(bloginfo('url')!=$_SERVER['REQUEST_URI']) { ?><h1><?php the_title(); ?></h1><? } ?>
 
 		<div class="entry">
 			<?php the_content('Yazının kalanını okuyun &raquo;'); ?>

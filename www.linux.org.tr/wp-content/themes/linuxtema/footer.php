042b408a498aedc4417c3b4ae2eb288e70fc1505
erdem.artan <erdem.artan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2011-08-17 09:37:24 +0000

    FIX#1088

--- www.linux.org.tr/wp-content/themes/linuxtema/footer.php
+++ www.linux.org.tr/wp-content/themes/linuxtema/footer.php
@@ -1,6 +1,6 @@
 <div class="wrapper" id="footer">
   <p class="left">
-    <a href='http://www.mozilla.org/firefox?WT.mc_id=aff_en06&WT.mc_ev=click'><img src='http://www.mozilla.org/contribute/buttons/110x32bubble_o.png' alt='Daha İyi Bir Web Deneyimi İçin Firefox Kullanın' border='0' /></a>
+    <a href='http://www.mozilla.org/firefox?WT.mc_id=aff_en06&WT.mc_ev=click'><img src='http://www.linux.org.tr/wp-content/themes/linuxtema/images/download-firefox.png' alt='Daha İyi Bir Web Deneyimi İçin Firefox Kullanın' border='0' /></a>
     <a href='http://www.wordpress.org/'><img src='http://www.linux.org.tr/wp-content/themes/linuxtema/images/wp-logo.png' alt='Site Alt Yapısı Olarak Wordpress Kullanılmaktadır.' border='0' /></a>
   </p>
   <span><a href="<?php bloginfo('siteurl'); ?>/gpl">GPL</a></span>

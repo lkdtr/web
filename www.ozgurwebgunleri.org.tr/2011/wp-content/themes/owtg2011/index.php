8217a71451cf83d87e03f6167eba208c037bde2b
Umuthan Uyan <mail@umuthan.com>
2011-08-17 21:24:10 +0000

    content style

--- www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/index.php
+++ www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/index.php
@@ -1,23 +1,21 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-
-<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	<? if($_SERVER['REQUEST_URI']!="/2011/") { ?><h1><?php the_title(); ?></h1><? } ?>
-
-		<div class="entry">
-			<?php the_content('Yazının kalanını okuyun &raquo;'); ?>
-		</div>
-
+<div id="content">
+	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
+			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<? if($_SERVER['REQUEST_URI']!="/2011/") { ?><h1><?php the_title(); ?></h1><? } ?>
+				<div class="entry">
+					<?php the_content('Yazının kalanını okuyun &raquo;'); ?>
+				</div>
+			</div>
+		<?php endwhile; ?>
+	<?php else : ?>
+	<h2 class="center">Bulunamadı</h2>
+		<p class="center">Üzgünüz, aradığınız şey burada değil.</p>
+		<?php get_search_form(); ?>
+	<?php endif; ?>
 </div>
-<?php endwhile; ?>
-
-<?php else : ?>
-<h2 class="center">Bulunamadı</h2>
-	<p class="center">Üzgünüz, aradığınız şey burada değil.</p>
-	<?php get_search_form(); ?>
-<?php endif; ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>

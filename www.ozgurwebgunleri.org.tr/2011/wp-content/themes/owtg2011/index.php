7911afdcb5a846a718185fd97111b6c7b768ada4
Umuthan Uyan <mail@umuthan.com>
2011-08-17 21:14:25 +0000

    index ve stil fix

--- www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/index.php
+++ www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/index.php
@@ -4,7 +4,7 @@
 <?php while (have_posts()) : the_post(); ?>
 
 <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	<? if(bloginfo('url')!=$_SERVER['REQUEST_URI']) { ?><h1><?php the_title(); ?></h1><? } ?>
+	<? if($_SERVER['REQUEST_URI']!="/2011/") { ?><h1><?php the_title(); ?></h1><? } ?>
 
 		<div class="entry">
 			<?php the_content('Yazının kalanını okuyun &raquo;'); ?>

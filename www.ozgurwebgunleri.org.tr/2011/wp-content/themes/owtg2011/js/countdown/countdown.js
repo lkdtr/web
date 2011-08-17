30433a1bb7544ab48156f2f61ba00165676ed724
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2011-08-17 19:52:18 +0000

    stil fix 2

--- www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/js/countdown/countdown.js
+++ www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/js/countdown/countdown.js
@@ -3,22 +3,5 @@ $(document).ready(
                 var cddate = new Date();
                 cddate = new Date(2011,10-1,14,10,0,0);
                 $('#countdown').countdown({until: cddate});
-
-                $("#countdown").delay(2000).fadeOut("slow",function(){
-                        $("#realdate").fadeIn("slow");
-                });
-
-                $("#date").hover(
-                        function(){
-                                $("#realdate").fadeOut("fast",function(){
-                                        $("#countdown").fadeIn("fast");
-                                });
-                        },
-                        function(){
-                                $("#countdown").fadeOut("fast",function(){
-                                        $("#realdate").fadeIn("fast");
-                                });
-                        }
-                );
         }
 );

448e151783a3125a3dac339c9468e19c800bafae
erdem.artan <erdem.artan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2011-11-04 09:54:06 +0000

    Sitenin statik surumu geliyor, ona yer acalim

--- www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/js/countdown/countdown.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(
-        function(){
-                var cddate = new Date();
-                cddate = new Date(2011,10-1,14,10,0,0);
-                $('#countdown').countdown({until: cddate});
-        }
-);
--- www.ozgurwebgunleri.org.tr/2011/wp-content/themes/owtg2011/sidebar.php
+++
@@ -1,4 +0,0 @@
-<div id="sidebar">
-	<?php wp_list_bookmarks('orderby=rating'); ?>
-	<?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
-</div>

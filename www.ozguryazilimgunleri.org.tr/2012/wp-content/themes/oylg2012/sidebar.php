83d26ec0af2f4450ac909b6fc561201f63ce7d0d
bahri.meric.canli <bahri.meric.canli@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2012-03-05 10:50:37 +0000

    sponsor logo

--- www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
@@ -1,5 +1,7 @@
 <div id="sidebar">
 	<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
 
-<b>Banner</b>
+	<div align="center">
+		<a href="http://www.ebiuniverse.com/"><img src="/2012/wp-content/themes/oylg2012/images/ebi-150.png" width="150px" height="74px" border="0" /></a>
+	</div>
 </div>

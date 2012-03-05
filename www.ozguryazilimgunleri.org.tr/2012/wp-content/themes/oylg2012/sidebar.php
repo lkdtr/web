87351bd9ead0f67358d7bd6dfa981b2a912bd104
Bahri Meriç CANLI <bahri@bahri.info>
2012-03-05 10:56:42 +0000

    aralarını açalım

--- www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
@@ -6,17 +6,17 @@
 </br>
 </br>
 </br>
-
+</br>
 
 		<a href="http://www.gittigidiyor.com/"><img src="/2012/wp-content/themes/oylg2012/images/gitti-gidiyor-150.png" width="150px" height="46px" border="0" /></a>
 </br>
-
-
+</br>
+</br>
 
 		<a href="http://www.obss.com.tr/"><img src="/2012/wp-content/themes/oylg2012/images/obss-150.png" width="150px" height="71px" border="0" /></a>
 </br>
-
-
+</br>
+</br>
 
 		<a href="http://www.ebiuniverse.com/"><img src="/2012/wp-content/themes/oylg2012/images/sahibinden-150.png" width="150px" height="45px" border="0" /></a>
 </br>

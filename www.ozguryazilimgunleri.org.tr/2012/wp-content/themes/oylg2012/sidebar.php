8175f467ec7a875634ed19c6cb7012b75251f9ec
Bahri Meriç CANLI <bahri@bahri.info>
2012-03-05 11:01:10 +0000

    logoları biraz daha ayıralım

--- www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
@@ -1,8 +1,8 @@
 <div id="sidebar">
-	<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-
 	<div align="center">
 		<a href="http://www.ebiuniverse.com/"><img src="/2012/wp-content/themes/oylg2012/images/ebi-150.png" width="150px" height="74px" border="0" /></a>
+</br>
+</br>
 </br>
 </br>
 </br>
@@ -12,17 +12,27 @@
 </br>
 </br>
 </br>
+</br>
+</br>
 
 		<a href="http://www.obss.com.tr/"><img src="/2012/wp-content/themes/oylg2012/images/obss-150.png" width="150px" height="71px" border="0" /></a>
+</br>
+</br>
 </br>
 </br>
 </br>
 
 		<a href="http://www.ebiuniverse.com/"><img src="/2012/wp-content/themes/oylg2012/images/sahibinden-150.png" width="150px" height="45px" border="0" /></a>
 </br>
+</br>
+</br>
+</br>
+</br>
 
 
 
 
 	</div>
+	<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
+
 </div>

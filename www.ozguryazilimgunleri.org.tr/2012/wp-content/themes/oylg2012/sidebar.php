9e36383392a9502cae734575d52e5d43321cbb52
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2012-03-13 22:08:25 +0000

    markafoni logosunu transparan olana kadar kaldiralim

--- www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
+++ www.ozguryazilimgunleri.org.tr/2012/wp-content/themes/oylg2012/sidebar.php
@@ -29,15 +29,6 @@
 </br>
 </br>
 
-		 <a href="http://www.markafoni.com/"><img src="/2012/wp-content/themes/oylg2012/images/markafoni-150.png" width="150px" height="56px" border="0" ></a>
-</br>
-</br>
-</br>
-
-
-
-
-
 	</div>
 	<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
 

9264ec0d599c4332cbb11460cdd3c16da3a0c52f
Umuthan Uyan <mail@umuthan.com>
2012-05-27 21:16:56 +0000

    #1667 tema duzeltme

--- kamp.linux.org.tr/2012/wp-content/themes/lkd_kamp/index.php
+++ kamp.linux.org.tr/2012/wp-content/themes/lkd_kamp/index.php
@@ -5,9 +5,7 @@
 		<?php while (have_posts()) : the_post(); ?>
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<h1><?php the_title(); ?></h1>
-			<div id="slider">
 				<?php lkdkamp_list_images(get_the_ID()); ?>
-			</div>
 				<div class="entry">
 					<?php the_content(); ?>
 				</div>

48aab9f3a87ebd5d9e2220c03a95696596c50466
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2012-06-15 13:36:49 +0000

    listeye cok birlesikti sponsor dosyasi ayrildi

--- kamp.linux.org.tr/2012/wp-content/themes/lkd_kamp/index.php
+++ kamp.linux.org.tr/2012/wp-content/themes/lkd_kamp/index.php
@@ -11,6 +11,7 @@
 				</div>
 				<div id="right">
 					<?php lkdkamp_list_pages(get_the_ID()); ?>
+					<div class="clear"></div>
 					<?php dynamic_sidebar('right'); ?>
 				</div>
 				<div class="clear"></div>

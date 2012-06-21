499db06e4b06f96a056eeeb4b51ea14f4e20ee6a
erdem.artan <erdem.artan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2012-06-21 14:14:03 +0000

    galeride kenar çubuğunda 'resim varsa iletişime geçin' ibaresininin eklenmesi fix: #305

--- galeri.linux.org.tr/themes/wind/views/sidebar.html.php
+++ galeri.linux.org.tr/themes/wind/views/sidebar.html.php
@@ -1,5 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <?= $theme->sidebar_top() ?>
+
 <div id="g-view-menu" class="g-buttonset ui-helper-clearfix">
   <? if ($page_subtype == "album"):?>
     <?= $theme->album_menu() ?>
@@ -11,6 +12,11 @@
     <?= $theme->tag_menu() ?>
   <? endif ?>
 </div>
-
+<div class="g-block">
+  <h2>Fotoğraf Gönderin</h2>
+  <div style="color:#5382bf; font-weight:bold; text-align:center">
+    Elinizde LKD etkinlikleri ile ilgili resimler mi var?<br> Yüklemek için web@linux.0rg.tr adresi ile iletişime geçebilirsiniz.
+  </div>
+</div>
 <?= $theme->sidebar_blocks() ?>
 <?= $theme->sidebar_bottom() ?>

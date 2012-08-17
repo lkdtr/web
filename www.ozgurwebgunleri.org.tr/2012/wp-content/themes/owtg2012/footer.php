08f3337b3e752791bb3d5740bb11af755476906c
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2012-08-17 20:52:24 +0000

    2012 sitesi tema ekleme denemesi. owtg2012 temasi, owtg2011'in aynisi

---
+++ www.ozgurwebgunleri.org.tr/2012/wp-content/themes/owtg2012/footer.php
@@ -0,0 +1,16 @@
+</div>
+
+<div class="clearfix"></div>
+
+<div id="footer"></div>
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>

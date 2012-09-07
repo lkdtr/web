3cd4dbf2b55c83d8769fee9fbb711c1db297e7b6
Umuthan Uyan <mail@umuthan.com>
2012-09-07 06:40:56 +0000

    owtg 2012 theme v1.0

--- www.ozgurwebgunleri.org.tr/2012/wp-content/themes/owtg2012/footer.php
+++ www.ozgurwebgunleri.org.tr/2012/wp-content/themes/owtg2012/footer.php
@@ -1,16 +1,31 @@
-</div>
+</section>
 
 <div class="clearfix"></div>
 
-<div id="footer"></div>
+<footer id="footer">
+	<section id="wrapper">
+		<section id="followus">
+			<h3>TAKİP ET</h3>
+			<ul id="followus" class="social">
+				<li class="linkedin"><a href="http://events.linkedin.com/Ozgur-Web-Teknolojileri-Gunleri/pub/383023" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="facebook"><a href="http://www.facebook.com/event.php?eid=139940079428957" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="twitter"><a href="http://twitter.com/ozgurwebgunleri" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="technorati"><a href="http://identi.ca/ozgurwebgunleri" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+			</ul>
+		</section>
+		<section id="shareus">
+			<h3>PAYLAŞ</h3>
+			<ul class="social">
+				<li class="facebook"><a target="_blank" href="http://www.facebook.com/sharer.php?u=http://www.ozgurwebgunleri.org.tr/2012/&t=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="twitter"><a target="_blank" href="http://twitter.com/home?status=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde%20http://www.ozgurwebgunleri.org.tr/2012"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="linkedin"><a target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.ozgurwebgunleri.org.tr/2012&title=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%202012&summary=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde&source=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%202012"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+			</ul>
+		</section>
+		<div class="clearfix"></div>
+	</section>
+</footer>
 
-<?php
-	/* Always have wp_footer() just before the closing </body>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to reference JavaScript files.
-	 */
+<?php wp_footer(); ?>
 
-	wp_footer();
-?>
 </body>
 </html>
--- www.ozgurwebgunleri.org.tr/2012/wp-content/themes/owtg2012/sidebar.php
+++ www.ozgurwebgunleri.org.tr/2012/wp-content/themes/owtg2012/sidebar.php
@@ -1,4 +1,9 @@
-<div id="sidebar">
-	<?php wp_list_bookmarks('orderby=rating'); ?>
-	<?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
-</div>
+<section id="sidebar">
+	<div id="logo">
+		<a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a>
+	</div>
+	<?php wp_nav_menu(); ?>
+	<div id="countdown"></div>
+	<div class="clearfix"></div>
+	<?php dynamic_sidebar( 'left' ); ?>
+</section>

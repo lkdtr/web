036cbb9e58e4bc52fe63518bd7f9fb5a9d7ffa6e
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2013-03-04 09:19:18 +0000

    facebook sharing duzeltildi

--- www.ozguryazilimgunleri.org.tr/2013/wp-content/themes/oylg2013/footer.php
+++ www.ozguryazilimgunleri.org.tr/2013/wp-content/themes/oylg2013/footer.php
@@ -23,9 +23,9 @@
 			<span>SANTRAL KAMPÜSÜ</span>
 		</div>
 		<div id="share">
-			<a href="https://www.facebook.com/dialog/feed?link=http://www.ozguryazilimgunleri.org.tr/2013& name=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri& caption=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri& description=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri%202013& message=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri%205-6%20Nisan%20tarihlerinde%20%C4%B0stanbul%20Bilgi%20%C3%9Cniversitesi%20Santral%20Kamp%C3%BCs%C3%BCnde!& redirect_uri=http://www.ozguryazilimgunleri.org.tr/2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" /></a>
-			<a href="https://twitter.com/intent/tweet?source=webclient&text=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri%205-6%20Nisan%20tarihlerinde%20%C4%B0stanbul%20Bilgi%20%C3%9Cniversitesi%20Santral%20Kamp%C3%BCs%C3%BCnde!&url=http://www.ozguryazilimgunleri.org.tr/2013&hashtags=oylg2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" /></a>
-			<a href="http://identi.ca/ozguryazilimgunleri" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="" /></a>
+			<a href="https://www.facebook.com/share/sharer.php?u=http://www.ozguryazilimgunleri.org.tr/2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="55px" height="100px" /></a>
+			<a href="https://twitter.com/intent/tweet?source=webclient&text=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri%205-6%20Nisan%20tarihlerinde%20%C4%B0stanbul%20Bilgi%20%C3%9Cniversitesi%20Santral%20Kamp%C3%BCs%C3%BCnde!&url=http://www.ozguryazilimgunleri.org.tr/2013&hashtags=oylg2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="65px" height="100px" /></a>
+			<a href="http://identi.ca/ozguryazilimgunleri" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="70px" height="100px" /></a>
 		</div>
 		<div class="clear"></div>
 	</aside>

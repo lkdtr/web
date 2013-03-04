6ed7120d8a090228f12335aab15435cd8ab71d61
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2013-03-04 09:43:25 +0000

    facebook sharing duzeltildi

--- www.ozguryazilimgunleri.org.tr/2013/wp-content/themes/oylg2013/footer.php
+++ www.ozguryazilimgunleri.org.tr/2013/wp-content/themes/oylg2013/footer.php
@@ -23,7 +23,7 @@
 			<span>SANTRAL KAMPÜSÜ</span>
 		</div>
 		<div id="share">
-			<a href="https://www.facebook.com/share/sharer.php?u=http://www.ozguryazilimgunleri.org.tr/2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="55px" height="100px" /></a>
+			<a href="https://www.facebook.com/sharer.php?u=http://www.ozguryazilimgunleri.org.tr/2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="55px" height="100px" /></a>
 			<a href="https://twitter.com/intent/tweet?source=webclient&text=%C3%96zg%C3%BCr%20Yaz%C4%B1l%C4%B1m%20ve%20Linux%20G%C3%BCnleri%205-6%20Nisan%20tarihlerinde%20%C4%B0stanbul%20Bilgi%20%C3%9Cniversitesi%20Santral%20Kamp%C3%BCs%C3%BCnde!&url=http://www.ozguryazilimgunleri.org.tr/2013&hashtags=oylg2013" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="65px" height="100px" /></a>
 			<a href="http://identi.ca/ozguryazilimgunleri" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/blank.png" width="70px" height="100px" /></a>
 		</div>

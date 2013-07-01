a2b0383f356c2e347a41989cbf1d8dfe0f4c2bc6
Umuthan Uyan <mail@umuthan.com>
2013-07-01 07:34:14 +0000

    arghh şifre değil parola

--- kamp.linux.org.tr/2013/wp-content/themes/lkd_kamp/functions.php
+++ kamp.linux.org.tr/2013/wp-content/themes/lkd_kamp/functions.php
@@ -106,7 +106,7 @@ function lkdkamp_show_extra_fields(){
     <script type="text/javascript">
     	jQuery(document).ready(function(){
     		jQuery(".register").html("Kampa başvurmak için aşağıdaki formu doldurunuz.");
-    		jQuery("p#reg_passmail").html("Başvurunuzu son katılım tarihinden önce güncellemek için e-posta adresinize bir şifre gelecektir.");
+    		jQuery("p#reg_passmail").html("Başvurunuzu son katılım tarihinden önce güncellemek için e-posta adresinize bir parola gelecektir.");
     		jQuery("#github").hide();
     		if(jQuery("#user_education").indexOf("Java")==-1) jQuery("#github").show();
     	});
@@ -303,3 +303,20 @@ function lkdkamp_registration_redirect()
     return home_url( '/kamp-basarili-kayit' );
 }
 add_filter( 'registration_redirect', 'lkdkamp_registration_redirect' );
+
+function my_login_redirect( $redirect_to, $request, $user ){
+    //is there a user to check?
+    if( isset( $user->roles ) && is_array( $user->roles ) ) {
+        //check for admins
+        if( in_array( "administrator", $user->roles ) ) {
+            // redirect them to the default place
+            return $redirect_to;
+        } else {
+            return home_url();
+        }
+    }
+    else {
+        return $redirect_to;
+    }
+}
+add_filter("login_redirect", "my_login_redirect", 10, 3);

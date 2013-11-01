21eb54200702264e2bc03e5bcdb0178dbf58b5a8
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2013-11-01 11:05:34 +0000

    yil degistirelim

--- www.ozgurwebgunleri.org.tr/2013/wp-content/themes/owtg2012/footer.php
+++ www.ozgurwebgunleri.org.tr/2013/wp-content/themes/owtg2012/footer.php
@@ -16,9 +16,9 @@
 		<section id="shareus">
 			<h3>PAYLAŞ</h3>
 			<ul class="social">
-				<li class="facebook"><a target="_blank" href="http://www.facebook.com/sharer.php?u=http://www.ozgurwebgunleri.org.tr/2012/&t=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
-				<li class="twitter"><a target="_blank" href="http://twitter.com/home?status=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde%20http://www.ozgurwebgunleri.org.tr/2012"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
-				<li class="linkedin"><a target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.ozgurwebgunleri.org.tr/2012&title=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%202012&summary=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde&source=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%202012"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="facebook"><a target="_blank" href="http://www.facebook.com/sharer.php?u=http://www.ozgurwebgunleri.org.tr/2013/&t=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="twitter"><a target="_blank" href="http://twitter.com/home?status=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde%20http://www.ozgurwebgunleri.org.tr/2013"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
+				<li class="linkedin"><a target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.ozgurwebgunleri.org.tr/2013&title=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%202012&summary=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%2019-20%20Ekim%202012%27de%20Yeditepe%20%C3%9Cniversitesi%27nde&source=%C3%96zg%C3%BCr%20Web%20Teknoloji%20G%C3%BCnleri%202012"><img src="<?php bloginfo('template_url'); ?>/images/blank.gif" /></a></li>
 			</ul>
 		</section>
 		<div class="clearfix"></div>

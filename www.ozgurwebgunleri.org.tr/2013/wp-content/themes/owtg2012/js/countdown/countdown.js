79fb30c04e8e03c3d40e2f63003c0c72bc9753e9
doruk.fisek <doruk.fisek@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2013-11-19 05:51:35 +0000

    sayaci duzeltelim, yeni yilin tarihini koyalim

--- www.ozgurwebgunleri.org.tr/2013/wp-content/themes/owtg2012/js/countdown/countdown.js
+++ www.ozgurwebgunleri.org.tr/2013/wp-content/themes/owtg2012/js/countdown/countdown.js
@@ -2,7 +2,7 @@
 	$(document).ready(
         function(){
                 var cddate = new Date();
-                cddate = new Date(2012,10-1,19,10,0,0);
+                cddate = new Date(2013,11-1,22,10,0,0);
                 $('#countdown').countdown({until: cddate});
         }
 );

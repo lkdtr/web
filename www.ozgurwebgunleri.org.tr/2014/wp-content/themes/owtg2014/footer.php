a32b6b75ff91e936c1abfff55ca41a2832db54e2
merve.yalcin <merve.yalcin@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2014-10-21 20:25:02 +0000

    Footer'a twitter duyuru metni eklendi

--- www.ozgurwebgunleri.org.tr/2014/wp-content/themes/owtg2014/footer.php
+++ www.ozgurwebgunleri.org.tr/2014/wp-content/themes/owtg2014/footer.php
@@ -1,7 +1,9 @@
-<footer>
-      <div class="footer">
-        <img src="<?php echo bloginfo('template_directory') ?>/images/date.png" class="date" alt="" />
-      </div>
-    </footer>
-  </body>
-</html>
+<footer>       
+	<div class="footer">
+		<p class="dipnot">Bu yıl beşincisi
+düzenlenecek olan Özgür Web Teknolojileri Günleri 05 - 06 Aralık'ta İstanbul
+Yeditepe Üniversitesi'nde olacak! #owtg2014</p>         
+		<img src="<?php echo bloginfo('template_directory') ?>/images/date.png" class="date" alt="" />
+	</div>     
+</footer>   
+</body> </html>

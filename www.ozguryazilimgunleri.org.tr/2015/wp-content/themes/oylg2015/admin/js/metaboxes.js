0fb49ed29f5e80dc1ff44fbd2f981a99526d678d
umuthan.uyan <umuthan.uyan@21fd8ee0-a65d-40bb-b93d-96c9cd163f3a>
2015-02-18 12:53:06 +0000

    homepage template duzenleme

--- www.ozguryazilimgunleri.org.tr/2015/wp-content/themes/oylg2015/admin/js/metaboxes.js
+++ www.ozguryazilimgunleri.org.tr/2015/wp-content/themes/oylg2015/admin/js/metaboxes.js
@@ -1,8 +1,8 @@
-jQuery(document).ready(function($) {	
-	
+jQuery(document).ready(function($) {
+
 	var $template_select = jQuery('select#page_template'),
 		$template_box = jQuery('#normal-sortables');
-		
+
 	$template_select.change(function() {
 		var this_value = jQuery(this).val();
 		$('#slogan').css('display','none');
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
 		$('#social_link').css('display','none');
 		$('#postdivrich').css('display','none');
 		$('#link_page').css('display','none');
-		
+
 		switch ( this_value ) {
 			case 'default':
 				$('#postdivrich').css('display','block');
@@ -23,6 +23,7 @@ jQuery(document).ready(function($) {
 				$('#slogan').css('display','block');
 				$('#home_link').css('display','block');
 				$('#link_page').css('display','block');
+				$('#postdivrich').css('display','block');
 				break;
 			case 'templates/template-staff.php':
 				$('#staff_content').css('display','block');
@@ -52,6 +53,6 @@ jQuery(document).ready(function($) {
 				$('#link_page').css('display','none');
 		}
 	});
-	
+
 	$template_select.trigger('change');
 });

c2f9912a0214883476290a2234e1021e05e6ad42
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2016-05-05 17:35:41 +0000

    yeni etkinlik baglantisi

--- kamp.linux.org.tr/2016/wp-content/themes/lkd_kamp/footer.php
+++ kamp.linux.org.tr/2016/wp-content/themes/lkd_kamp/footer.php
@@ -7,7 +7,7 @@
 			<?php dynamic_sidebar('tweets'); ?>
 		</div>
 		<ul id="social">
-			<li><a href="https://www.facebook.com/events/585424441480433/" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/facebook_event_logo.png" /></a></li>
+			<li><a href="https://www.facebook.com/events/997111153698421/" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/facebook_event_logo.png" /></a></li>
 			<li><a href="https://www.facebook.com/linuxyazkampi" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/facebook_logo.png" /></a></li>
 			<li><a href="http://twitter.com/linuxyazkampi" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/images/twitter_logo.png" /></a></li>
 		</ul>
